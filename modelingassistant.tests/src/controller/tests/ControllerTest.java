f90e81dbb8173e0549ca3f00e10396159dc29d41
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2020-12-22 13:20:48 -0500

    Added trivial JUnit test

---
+++ modelingassistant.tests/src/controller/tests/ControllerTest.java
@@ -0,0 +1,12 @@
+package controller.tests;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class ControllerTest {
+  
+  @Test public void testJunitIsWorking() {
+    assertTrue(true);
+  }
+  
+}
