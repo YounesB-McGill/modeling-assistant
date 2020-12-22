00cec9bf0fc9a7f25971979204f0cbbebe4166f7
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2020-12-22 15:20:17 -0500

    Add test for creating one class solution

--- modelingassistant.tests/src/controller/tests/ControllerTest.java
+++ modelingassistant.tests/src/controller/tests/ControllerTest.java
@@ -24,4 +24,46 @@ public class ControllerTest {
     assertEquals("Student1_solution", modelingAssistant.getSolutions().get(0).getClassDiagram().getName());
   }
   
+  /**
+   * Tests that a solution with these one class can be created (Umple syntax):
+   * 
+   * class Car {
+   *   int id;
+   *   String make; // eg, BMW, Honda
+   * }
+   */
+  @Test public void testCreatingOneClassSolution() {
+    var maf = ModelingassistantFactory.eINSTANCE;
+    var cdf = ClassdiagramFactory.eINSTANCE;
+    var modelingAssistant = maf.createModelingAssistant();
+    var solution = maf.createSolution();
+    var classDiagram = cdf.createClassDiagram();
+    classDiagram.setName("Student1_solution");
+    solution.setClassDiagram(classDiagram);
+    modelingAssistant.getSolutions().add(solution);
+    
+    var carClass = cdf.createClass();
+    carClass.setName("Car");
+    var carId = cdf.createAttribute();
+    carId.setName("id");
+    carId.setType(cdf.createCDInt());
+    var carMake = cdf.createAttribute();
+    carMake.setName("make");
+    carMake.setType(cdf.createCDString());
+    carClass.getAttributes().add(carId);
+    carClass.getAttributes().add(carMake);
+    
+    classDiagram.getClasses().add(carClass);
+    
+    assertEquals("Student1_solution", modelingAssistant.getSolutions().get(0).getClassDiagram().getName());
+    
+    assertEquals("Car", classDiagram.getClasses().get(0).getName());
+    assertEquals("id", classDiagram.getClasses().get(0).getAttributes().get(0).getName());
+    assertEquals("make", classDiagram.getClasses().get(0).getAttributes().get(1).getName());
+    
+    // These fail
+    // assertEquals(cdf.createCDInt(),  classDiagram.getClasses().get(0).getAttributes().get(0).getType());
+    // assertEquals(cdf.createCDString(),  classDiagram.getClasses().get(0).getAttributes().get(1).getType());
+  }
+  
 }
