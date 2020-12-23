3c18f78ec145636caadc1dd50a5bd9bd2eb783d9
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2020-12-23 17:17:02 -0500

    Update Python test for creating one class solution (passes)

--- modelingassistant/pythonclient/test_controller.py
+++ modelingassistant/pythonclient/test_controller.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from classdiagram.classdiagram import ClassDiagram, Class, Attribute, ImplementationClass
+from classdiagram.classdiagram import ClassDiagram, Class, Attribute, ImplementationClass, CDInt, CDString
 from modelingassistant.modelingassistant import ModelingAssistant, Solution
 from pyecore.ecore import EInteger, EString
 
@@ -24,21 +24,25 @@ def test_creating_one_class_solution():
     class_diagram = ClassDiagram(name="Student1_solution")
     solution.classDiagram = class_diagram
     modeling_assistant.solutions.append(solution)
+    
+    cd_int = CDInt()
+    cd_string = CDString()
 
     car_class = Class(name="Car", attributes=[
-        Attribute(name="id", type=ImplementationClass(dataType=EInteger)),
-        Attribute(name="make", type=ImplementationClass(dataType=EString))
+        Attribute(name="id", type=cd_int),
+        Attribute(name="make", type=cd_string)
     ])
     class_diagram.classes.append(car_class)
 
     assert "Student1_solution" == modeling_assistant.solutions[0].classDiagram.name
     assert "Car" == class_diagram.classes[0].name
     assert "id" == class_diagram.classes[0].attributes[0].name
-    assert EInteger == class_diagram.classes[0].attributes[0].type.dataType
+    assert cd_int == class_diagram.classes[0].attributes[0].type
     assert "make" == class_diagram.classes[0].attributes[1].name
-    assert EString == class_diagram.classes[0].attributes[1].type.dataType
+    assert cd_string == class_diagram.classes[0].attributes[1].type
 
 
 if __name__ == "__main__":
     "Main entry point."
     test_creating_empty_solution()
+    test_creating_one_class_solution()
