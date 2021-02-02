4f969b61714199814ba8750899ed71fc9154e7e6
Prabhsimran Singh <72397599+Prabhsimran-Singh@users.noreply.github.com>
2021-02-02 20:06:11 +0530

    Added Function "checkMistakeSoftwareEngineeringTerm"

--- mistakedetection/src/ca/mcgill/sel/mistakedetection/MistakeDetection.java
+++ mistakedetection/src/ca/mcgill/sel/mistakedetection/MistakeDetection.java
@@ -2,15 +2,39 @@ package ca.mcgill.sel.mistakedetection;
 
 import classdiagram.ClassdiagramFactory;
 import classdiagram.Classifier;
+
+import java.util.HashMap;
 import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+
 import modelingassistant.Mistake;
 import modelingassistant.ModelingassistantFactory;
+import modelingassistant.SolutionElement;
+import org.eclipse.emf.common.util.EList;
 
 public class MistakeDetection {
   
   public static final ClassdiagramFactory CDF = ClassdiagramFactory.eINSTANCE;
   public static final ModelingassistantFactory MAF = ModelingassistantFactory.eINSTANCE;
 
+  EList<Mistake> mistakes;
+  HashMap<Classifier, Classifier> mappedClassifier = new HashMap<Classifier, Classifier>();
+  
+  public void checkMistakeSoftwareEngineeringTerm(Classifier sClassifier) {
+	  if (isSoftwareEngineeringTerm(sClassifier.getName())) {
+//		  Mistake m = new Mistake("Class Name contains software engineering term");
+//	      SolutionElement s = new SolutionElement(sClassifier);
+//	      m.getSolutionElements().add(s);
+//	      mistakes.add(m);
+		  SolutionElement s = null;
+		  s.setElement(sClassifier);
+		  Mistake m = MistakeTypes.SOFTWARE_ENGINEERING_TERM_M;
+		  m.getSolutionElements().add(s);
+		  mistakes.add(m);
+		  
+	      }
+  }
   /**
    * Returns true if the input string is a software engineering term.
    */
