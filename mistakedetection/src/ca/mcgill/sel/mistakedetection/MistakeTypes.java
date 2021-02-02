3fbcc3c5a1de4667eb4764ee96e6483c551e52c9
Prabhsimran Singh <72397599+Prabhsimran-Singh@users.noreply.github.com>
2021-02-02 20:06:11 +0530

    Added Function "checkMistakeSoftwareEngineeringTerm"

--- mistakedetection/src/ca/mcgill/sel/mistakedetection/MistakeTypes.java
+++ mistakedetection/src/ca/mcgill/sel/mistakedetection/MistakeTypes.java
@@ -1,5 +1,6 @@
 package ca.mcgill.sel.mistakedetection;
 
+import modelingassistant.Mistake;
 import modelingassistant.MistakeType;
 import modelingassistant.ModelingassistantFactory;
 
@@ -9,9 +10,16 @@ public class MistakeTypes {
   
   public static final MistakeType PLURAL_CLASS_NAME = MAF.createMistakeType();
   
+  public static final MistakeType SOFTWARE_ENGINEERING_TERM = MAF.createMistakeType();
+  
+  public static final Mistake SOFTWARE_ENGINEERING_TERM_M = MAF.createMistake();
+  
   static {
     PLURAL_CLASS_NAME.setName("Class name is plural");
     PLURAL_CLASS_NAME.setAtomic(true);
+    
+    SOFTWARE_ENGINEERING_TERM.setName("Class name contains software engineering term");
+    SOFTWARE_ENGINEERING_TERM.setAtomic(true);
   }
   
 }
