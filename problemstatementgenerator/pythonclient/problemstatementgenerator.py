37dace81a37cec1cdfbb1736cf80caa43084c725
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-02-07 18:44:02 -0500

    Add modelingassistant imports

--- problemstatementgenerator/pythonclient/problemstatementgenerator.py
+++ problemstatementgenerator/pythonclient/problemstatementgenerator.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+
+import modelingassistant_imports
+from modelingassistant.pythonclient.classdiagram import ClassDiagram
+from pyecore.resources import ResourceSet, URI
+
+
 def main():
     cdm_mm_file = "modelingassistant/model/classdiagram.ecore"
     rset = ResourceSet()
@@ -6,11 +13,17 @@ def main():
 
     # Open a class diagram instance
     cdm_path = "modelingassistant/testmodels"
-    cdm_file = f"{cdm_path}/car.domain_model.cdm"
+    cdm_file = f"{cdm_path}/car_sportscar_part_driver.domain_model.cdm"
     resource = rset.get_resource(URI(cdm_file))
     class_diagram = resource.contents[0]
     class_diagram.__class__ = ClassDiagram
 
+    print_classes(class_diagram)
+
+
+def print_classes(cd: ClassDiagram):
+    for clazz in cd.classes:
+        print(clazz.name)
 
 
 if __name__ == "__main__":
