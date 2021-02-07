d381f8d548b98b177e258c09cf2a323f700224d1
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-02-07 15:44:30 -0500

    Fix testCreatingEmptySolution()

--- modelingassistant.tests/src/controller/tests/ControllerTest.java
+++ modelingassistant.tests/src/controller/tests/ControllerTest.java
@@ -36,16 +36,16 @@ public class ControllerTest {
   @Test public void testJunitIsWorking() {
     assertTrue(true);
   }
-  
+
   @Test public void testCreatingEmptySolution() {
     var maf = ModelingassistantFactory.eINSTANCE;
     var cdf = ClassdiagramFactory.eINSTANCE;
     var modelingAssistant = maf.createModelingAssistant();
-    var solution = maf.createSolution();
     var classDiagram = cdf.createClassDiagram();
+    var solution = maf.createSolution();
     classDiagram.setName("Student1_solution");
+    solution.setModelingAssistant(modelingAssistant);
     solution.setClassDiagram(classDiagram);
-    modelingAssistant.getSolutions().add(solution);
     assertEquals("Student1_solution", modelingAssistant.getSolutions().get(0).getClassDiagram().getName());
   }
   
