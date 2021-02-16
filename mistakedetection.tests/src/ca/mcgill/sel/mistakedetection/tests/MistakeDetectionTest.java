32e04b6988cf1f1592fa47cf17b152b32fef18ac
Prabhsimran Singh <72397599+Prabhsimran-Singh@users.noreply.github.com>
2021-02-16 17:00:27 +0530

    Addition of Domain-Models and Tests

--- mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/MistakeDetectionTest.java
+++ mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/MistakeDetectionTest.java
@@ -1,13 +1,27 @@
 package ca.mcgill.sel.mistakedetection.tests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
-import java.io.IOException;
 import java.util.List;
-import ca.mcgill.sel.mistakedetection.MistakeDetection;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import ca.mcgill.sel.mistakedetection.MistakeDetection;
+import classdiagram.CDInt;
+import classdiagram.CDString;
+import classdiagram.ClassDiagram;
+import classdiagram.ClassdiagramPackage;
+import classdiagram.Classifier;
+import classdiagram.ReferenceType;
+import modelingassistant.ModelingassistantFactory;
+import modelingassistant.util.ResourceHelper;
 
 public class MistakeDetectionTest {
 
@@ -25,10 +39,10 @@ public class MistakeDetectionTest {
    */
   @Test public void testIsPlural() {
    
-    List.of("Car", "Driver", "Part","Bus").forEach(s -> assertFalse(MistakeDetection.isPlural(s)));
+    List.of("Car", "Driver").forEach(s -> assertFalse(MistakeDetection.isPlural(s)));
     
    
-    List.of("Cars", "Drivers","Parts","People","Men").forEach(s -> assertTrue(MistakeDetection.isPlural(s)));
+    List.of("Cars","People","Men").forEach(s -> assertTrue(MistakeDetection.isPlural(s)));
     
    
   }
@@ -47,5 +61,37 @@ public class MistakeDetectionTest {
     assertEquals(1, MistakeDetection.levenshteinDistance(class1, class2));
   }
   
-  
+  @Test public void CheckInstructorSolution() {
+	
+	  ClassdiagramPackage.eINSTANCE.eClass();
+	 var cdmFile = "../mistakedetection/testModels/InstructorSolution/One/ClassDiagram/InstructorSolution.domain_model.cdm";
+	 var resource = ResourceHelper.INSTANCE.loadResource(cdmFile);
+	 var classDiagram = (ClassDiagram) resource.getContents().get(0);
+	  
+	  
+	  	Classifier busClass = null;
+	    Classifier driverClass = null;
+	    
+	    for (var c: classDiagram.getClasses()) {
+	      if ("Bus".equals(c.getName())) busClass = c;
+	      else if ("Driver".equals(c.getName())) driverClass = c;
+	     
+	    }
+
+	    assertEquals(busClass, classDiagram.getClasses().get(0));
+	    assertEquals("Bus", busClass.getName());
+	    assertEquals(driverClass, classDiagram.getClasses().get(1));
+	    assertEquals("Driver", driverClass.getName());
+	    
+	    
+	    
+	    var maf = ModelingassistantFactory.eINSTANCE;
+	    var modelingAssistant = maf.createModelingAssistant();
+	    var solution = maf.createSolution();
+	    solution.setModelingAssistant(modelingAssistant);
+	    solution.setClassDiagram(classDiagram);
+	    
+	    List.of(busClass, driverClass).forEach(c ->
+	       assertTrue(solution.getClassDiagram().getClasses().contains(c)));
+  }
 }
