8612fc360383d67550e16991e474f6f6095bb4b6
Thomas Woodfine-MacPherson <woodfinemacpherson@gmail.com>
2021-02-22 18:53:10 -0500

    Added python client

--- problemstatementgenerator/pythonclient/problemstatementgenerator.py
+++ problemstatementgenerator/pythonclient/problemstatementgenerator.py
@@ -6,17 +6,21 @@ from pyecore.resources import ResourceSet, URI
 
 
 def main():
-    cdm_mm_file = "modelingassistant/model/classdiagram.ecore"
+    cdm_mm_file = "../../modelingassistant/model/classdiagram.ecore"
     rset = ResourceSet()
-    mm_root = rset.get_resource(URI(cdm_mm_file)).contents[0]
+    resource = rset.get_resource(URI(cdm_mm_file))
+    mm_root = resource.contents[0]
     rset.metamodel_registry[mm_root.nsURI] = mm_root  # ecore is loaded in the 'rset' as a metamodel here
 
     # Open a class diagram instance
-    cdm_path = "modelingassistant/testmodels"
+    cdm_path = "../../modelingassistant/testmodels"
     cdm_file = f"{cdm_path}/car_sportscar_part_driver.domain_model.cdm"
     resource = rset.get_resource(URI(cdm_file))
     class_diagram = resource.contents[0]
     class_diagram.__class__ = ClassDiagram
+
+    # massDir(class_diagram.classes[1])
+    # massDir(class_diagram)
 
     print_classes(class_diagram)
 
@@ -24,6 +28,28 @@ def main():
 def print_classes(cd: ClassDiagram):
     for clazz in cd.classes:
         print(clazz.name)
+
+        print_attributes(clazz)
+
+
+def print_attributes(cl):
+    print(tab(1), "Attr: ")
+    for att in cl.attributes:
+
+        print(tab(2), att.name)
+
+
+def tab(n: int):
+    s = "  "
+    for i in range(n):
+        s += s
+    return s
+
+
+def massDir(cd: ClassDiagram):
+    for val in dir(cd):
+        print(val.upper())
+        print(dir(val), "\n", "-----------------------------------------------------------")
 
 
 if __name__ == "__main__":
