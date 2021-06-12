91d328d8598fa6960ef791a7e7b2de5c38fa9ae1
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-06-12 01:28:56 -0400

    Add custom Python function for LC.mistakeTypes()

--- modelingassistant/pythonclient/generatepythoncode.py
+++ modelingassistant/pythonclient/generatepythoncode.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+from textwrap import dedent
 import ast
 import os
+
+from learningcorpus.learningcorpus import LearningCorpus
 
 def generate_pyecore():
     metamodel_names = ["classdiagram", "learningcorpus", "modelingassistant"]
@@ -11,17 +14,31 @@ def generate_pyecore():
 
 
 def customize_generated_code():
-    with open("modelingassistant/pythonclient/learningcorpus/learningcorpus.py") as lc:
+    # TODO Generalize this as needed
+    # Add the following function to the generated LearningCorpus class
+    lc_mistakeTypes_func = ast.parse(dedent("""\
+    def mistakeTypes(self) -> list:
+        "Custom function to return all the mistake types from their categories."
+        import itertools
+        return list(itertools.chain(*[mtc.mistakeTypes for mtc in self.mistakeTypeCategories]))
+    """))
+
+    lc_py = "modelingassistant/pythonclient/learningcorpus/learningcorpus.py"
+
+    # Open and parse file
+    with open(lc_py) as lc:
         lc_ast = ast.parse(lc.read())
         for e in lc_ast.body:
-            if "name" in dir(e) and e.name == "MistakeTypeCategory":
-
-                print(ast.dump(e))
-                print()
-        print(dir(e))
+            # Find the LearningCorpus class
+            if "name" in dir(e) and e.name == "LearningCorpus":
+                # Add the custom function to it
+                e.body.append(lc_mistakeTypes_func)
+    # Unparse the file back to a string and save it to file
+    with open(lc_py, "w") as lc:
+        lc.write(ast.unparse(lc_ast))
 
 
 if __name__ == "__main__":
     "Main entry point."
-    #generate_pyecore()
+    generate_pyecore()
     customize_generated_code()
