ce9bd693396dc8b41c48e8fae23a9e7c9cf26f36
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-06-20 23:02:01 -0400

    Add fileserdes module

--- modelingassistant/pythonclient/modelingassistant_client.py
+++ modelingassistant/pythonclient/modelingassistant_client.py
@@ -3,15 +3,8 @@
 import sys
 
 from pyecore.ecore import EClass, EPackage
-from pyecore.resources import ResourceSet, URI
 
 from modelingassistant.modelingassistant import Solution
-
-
-MM_PATH = "modelingassistant/model"
-CLASS_DIAGRAM_MM = "ca.mcgill.sel.classdiagram/model/classdiagram.ecore"
-LEARNING_CORPUS_MM = f"{MM_PATH}/learningcorpus.ecore"
-MODELING_ASSISTANT_MM = f"{MM_PATH}/modelingassistant.ecore"
 
 
 if sys.version_info[:2] < (3, 9):
@@ -26,17 +19,6 @@ def give_feedback(solution: Solution):
     return "Feedback"
 
 
-def load_metamodels(*ecore_files: str) -> ResourceSet:
-    """
-    Return a ResourceSet loaded with the given metamodels from the ecore file paths.
-    """
-    rset = ResourceSet()
-    for ecore_file in ecore_files:
-        mm_root = rset.get_resource(URI(ecore_file)).contents[0]
-        rset.metamodel_registry[mm_root.nsURI] = mm_root  # ecore loaded in rset as a metamodel here
-    return rset
-
-
 def get_classifier_by_name(metamodel_root: EPackage, name: str) -> EClass:
     for classifier in metamodel_root.eClassifiers:
         if classifier.name == name:
@@ -46,8 +28,4 @@ def get_classifier_by_name(metamodel_root: EPackage, name: str) -> EClass:
 
 if __name__ == '__main__':
     "Main entry point."
-
     print(f"Starting Modeling Assistant with Python {sys.version.split()[0]}")
-    metamodels = load_metamodels(["modelingassistant/model/classdiagram.ecore",
-                                  "modelingassistant/model/modelingassistant.ecore"])
-    print(metamodels)
