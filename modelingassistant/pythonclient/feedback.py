a08fb52ca0354127b528c21111fd0284c203ced6
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-06-24 02:42:11 -0400

    Add make_ma_with_1_mistake() and a simple test

--- modelingassistant/pythonclient/feedback.py
+++ modelingassistant/pythonclient/feedback.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from learningcorpus.learningcorpus import Feedback, TextResponse
-from modelingassistant.modelingassistant import FeedbackItem, Solution
+from learningcorpus.learningcorpus import TextResponse
+from modelingassistant.modelingassistant import FeedbackItem, Mistake, Solution
 
 def give_feedback(student_solution: Solution) -> FeedbackItem:
     "Give feedback on the given student solution."
@@ -10,8 +10,11 @@ def give_feedback(student_solution: Solution) -> FeedbackItem:
     if not student_solution.mistakes:
         # emoji to test serdes
         return FeedbackItem(feedback=TextResponse(text="All good, no mistakes found! 🎉"))
-    return None
 
+    mistakes = sorted(student_solution.mistakes, key=lambda m: m.mistakeType.priority)
+    highest_priority_mistake = mistakes[0]
+
+    return FeedbackItem(feedback=TextResponse(text=f"Found mistake of type {highest_priority_mistake.mistakeType.name}"))
 
 
 if __name__ == '__main__':
