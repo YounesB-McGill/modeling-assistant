63e28239d3b432d4968fe22c515285e213ec85a5
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-06-24 02:42:11 -0400

    Add make_ma_with_1_mistake() and a simple test

--- modelingassistant/pythonclient/test/test_feedback.py
+++ modelingassistant/pythonclient/test/test_feedback.py
@@ -2,15 +2,17 @@
 
 import os
 import sys
-from feedback import give_feedback
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from feedback import give_feedback
+from classdiagram.classdiagram import Class
 from learningcorpus.learningcorpus import Feedback, TextResponse
 from mistaketypes import SOFTWARE_ENGINEERING_TERM
-from modelingassistant.modelingassistant import ModelingAssistant, ProblemStatement, Solution, Student, StudentKnowledge
+from modelingassistant.modelingassistant import Mistake, ModelingAssistant, ProblemStatement, Solution, SolutionElement, Student, StudentKnowledge
 
 
-def make_ma_wo_mistakes() -> ModelingAssistant:
+def make_ma_without_mistakes() -> ModelingAssistant:
     "Make a simple Modeling Assistant instance with no mistakes."
     ma = ModelingAssistant()
     bus_ps = ProblemStatement(name="Bus Management System", modelingAssistant=ma)
@@ -21,12 +23,31 @@ def make_ma_wo_mistakes() -> ModelingAssistant:
     return ma
 
 
-def test_feedback_wo_mistakes():
-    solution = make_ma_wo_mistakes().solutions[0]
+def make_ma_with_1_mistake() -> ModelingAssistant:
+    "Make a simple Modeling Assistant instance with one mistake."
+    ma = ModelingAssistant()
+    bus_ps = ProblemStatement(name="Bus Management System", modelingAssistant=ma)
+    alice = Student(name="Alice", modelingAssistant=ma)
+    StudentKnowledge(mistakeType=SOFTWARE_ENGINEERING_TERM, student=alice, modelingAssistant=ma)
+    alice_bus_sol = Solution(student=alice, problemStatement=bus_ps, modelingAssistant=ma)
+    alice.currentSolution = alice_bus_sol
+    SolutionElement(solution=alice_bus_sol, element=Class(name="BusData"))
+    Mistake(studentSolution=alice_bus_sol, mistakeType=SOFTWARE_ENGINEERING_TERM)
+    return ma
+
+
+def test_feedback_without_mistakes():
+    solution = make_ma_without_mistakes().solutions[0]
     feedback_item = give_feedback(solution)
-    assert "no mistakes found" in feedback_item.feedback.text
+    assert "no mistakes found" in feedback_item.feedback.text.lower()
+
+
+def test_feedback_with_1_mistake():
+    solution = make_ma_with_1_mistake().solutions[0]
+    feedback_item = give_feedback(solution)
+    assert "software engineering term" in feedback_item.feedback.text.lower()
 
 
 if __name__ == '__main__':
     "Main entry point (used for debugging)."
-    make_ma_wo_mistakes()
+    test_feedback_with_1_mistake()
