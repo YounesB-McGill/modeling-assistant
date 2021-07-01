854e7e0f916abaf961020eddef0eb2a59e8f2bad
Prabhsimran Singh <72397599+Prabhsimran-Singh@users.noreply.github.com>
2021-07-02 02:18:34 +0530

    Added Assoc Detection

--- mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/MistakeDetectionWrongClassTest.java
+++ mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/MistakeDetectionWrongClassTest.java
@@ -18,6 +18,7 @@ import static modelingassistant.util.ResourceHelper.cdmFromFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ca.mcgill.sel.classdiagram.Classifier;
 import ca.mcgill.sel.mistakedetection.MistakeDetection;
