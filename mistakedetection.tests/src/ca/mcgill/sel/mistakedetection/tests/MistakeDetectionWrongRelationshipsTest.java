231cf194f98be4c49197d16a9ac29c50beff6403
fatma <85255648+fatmaalfalahi@users.noreply.github.com>
2021-07-07 00:51:42 +0400

    created 3 testcases 2 for attribute instead of association and 1 association instead of composition and added 6 cdm file 3 for instructor and 3 for student in  modeltotestRelationship

--- mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/MistakeDetectionWrongRelationshipsTest.java
+++ mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/MistakeDetectionWrongRelationshipsTest.java
@@ -2,14 +2,21 @@ package ca.mcgill.sel.mistakedetection.tests;
 
 import static ca.mcgill.sel.mistakedetection.tests.MistakeDetectionTest.assertMistake;
 import static ca.mcgill.sel.mistakedetection.tests.MistakeDetectionTest.getAssociationEndFromClass;
+import static ca.mcgill.sel.mistakedetection.tests.MistakeDetectionTest.getAssociationFromClassDiagram;
+import static ca.mcgill.sel.mistakedetection.tests.MistakeDetectionTest.getAttributeFromClass;
 import static ca.mcgill.sel.mistakedetection.tests.MistakeDetectionTest.getClassFromClassDiagram;
 import static ca.mcgill.sel.mistakedetection.tests.MistakeDetectionTest.instructorSolutionFromClassDiagram;
 import static ca.mcgill.sel.mistakedetection.tests.MistakeDetectionTest.studentSolutionFromClassDiagram;
 import static learningcorpus.mistaketypes.MistakeTypes.BAD_ROLE_NAME_SPELLING;
+import static learningcorpus.mistaketypes.MistakeTypes.USING_AN_ATTRIBUTE_INSTEAD_OF_AN_ASSOCIATION;
+import static learningcorpus.mistaketypes.MistakeTypes.USING_ASSOCIATION_INSTEAD_OF_AGGREGATION_COMPOSITION;
 import static modelingassistant.util.ResourceHelper.cdmFromFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import ca.mcgill.sel.classdiagram.Association;
 import ca.mcgill.sel.classdiagram.AssociationEnd;
+import ca.mcgill.sel.classdiagram.Attribute;
 import ca.mcgill.sel.classdiagram.Classifier;
 import ca.mcgill.sel.mistakedetection.MistakeDetection;
 import modelingassistant.Mistake;
@@ -63,4 +70,99 @@ public class MistakeDetectionWrongRelationshipsTest {
     }
   }
 
+  /**
+   * Test to check mistake of using Attribute instead of an association.
+   */
+  @Disabled
+  @Test
+  public void testMistakeAttributeInsteadOfAnAssociation() {
+    var instructorClassDiagram = cdmFromFile(
+        "../mistakedetection/testModels/InstructorSolution/ModelsToTestRelationship/instructor_busAndDriverClass/Class Diagram/Instructor_busAndDriverClass.domain_model.cdm");
+    var instructorSolution = instructorSolutionFromClassDiagram(instructorClassDiagram);
+
+    var studentClassDiagram = cdmFromFile(
+        "../mistakedetection/testModels/StudentSolution/ModelsToTestRelationship/student_attributeInsteadOfAssociation/Class Diagram/Student_attributeInsteadOfAssociation.domain_model.cdm");
+    var studentSolution = studentSolutionFromClassDiagram(studentClassDiagram);
+
+    var     instructorBusClass = getClassFromClassDiagram("Bus", instructorClassDiagram);
+    var     instructorDriverClass = getClassFromClassDiagram("Driver", instructorClassDiagram);
+
+    var     studentBusClass = getClassFromClassDiagram("Bus", studentClassDiagram);
+
+    Association instructorBusandDriverAssociation = getAssociationFromClassDiagram(instructorDriverClass , instructorBusClass,instructorClassDiagram);
+    Attribute studentdriverAttribute = getAttributeFromClass("driver", studentBusClass);
+
+    var comparison = MistakeDetection.compare(instructorSolution, studentSolution);
+
+    assertEquals(1, comparison.newMistakes.size());
+    assertEquals(1, studentSolution.getMistakes().size());
+
+    for (Mistake m : studentSolution.getMistakes()) {
+      assertMistake(m, USING_AN_ATTRIBUTE_INSTEAD_OF_AN_ASSOCIATION, studentdriverAttribute, instructorBusandDriverAssociation, 0, 1,
+          false);
+  }
+  }
+
+  /**
+   * Test to check mistake of using Attribute instead of an association.
+   */
+  @Disabled
+  @Test
+  public void testMistakeAttributeInsteadOfAnAssociation1() {
+    var instructorClassDiagram = cdmFromFile(
+        "../mistakedetection/testModels/InstructorSolution/ModelsToTestRelationship/instructor_pilotAndFlightClass/Class Diagram/Instructor_pilotAndFlightClass.domain_model.cdm");
+    var instructorSolution = instructorSolutionFromClassDiagram(instructorClassDiagram);
+
+    var studentClassDiagram = cdmFromFile(
+        "../mistakedetection/testModels/StudentSolution/ModelsToTestRelationship/student_attributeInsteadOfAssociation1/Class Diagram/Student_attributeInsteadOfAssociation1.domain_model.cdm");
+    var studentSolution = studentSolutionFromClassDiagram(studentClassDiagram);
+
+    var instructorPilotClass = getClassFromClassDiagram("Pilot", instructorClassDiagram);
+    var instructorFlightClass = getClassFromClassDiagram("Flight", instructorClassDiagram);
+
+    var studentPilotClass = getClassFromClassDiagram("Pilot", studentClassDiagram);
+
+    Association instructorPilotandFlightAssociation = getAssociationFromClassDiagram(instructorPilotClass , instructorFlightClass,instructorClassDiagram);
+    Attribute studentflightAttribute = getAttributeFromClass("flight", studentPilotClass);
+
+    var comparison = MistakeDetection.compare(instructorSolution, studentSolution);
+
+    assertEquals(1, comparison.newMistakes.size());
+    assertEquals(1, studentSolution.getMistakes().size());
+
+    for (Mistake m : studentSolution.getMistakes()) {
+      assertMistake(m, USING_AN_ATTRIBUTE_INSTEAD_OF_AN_ASSOCIATION, studentflightAttribute, instructorPilotandFlightAssociation, 0, 1,
+          false);
+  }
+  }
+
+  /**
+   * Test to check Missing composition.
+   */
+  @Disabled
+  @Test
+  public void testMistakeMissingComposition() {
+    var instructorClassDiagram = cdmFromFile(
+        "../mistakedetection/testModels/InstructorSolution/ModelsToTestRelationship/instructor_companyAndEmployeeClass/Class Diagram/Instructor_companyAndEmployeeClass.domain_model.cdm");
+    var instructorSolution = instructorSolutionFromClassDiagram(instructorClassDiagram);
+
+    var studentClassDiagram = cdmFromFile(
+        "../mistakedetection/testModels/StudentSolution/ModelsToTestRelationship/student_missingComposition/Class Diagram/Student_missingComposition.domain_model.cdm");
+    var studentSolution = studentSolutionFromClassDiagram(studentClassDiagram);
+
+    var  instructorEmployeeClass = getClassFromClassDiagram("Employee", instructorClassDiagram);
+    var  studentEmployeeClass = getClassFromClassDiagram("Employee", studentClassDiagram);
+    AssociationEnd instructorMyCompanyAssociationEnd = getAssociationEndFromClass("myCompany", instructorEmployeeClass);
+    AssociationEnd studentMyCompanyAssociationEnd = getAssociationEndFromClass("myCompany", studentEmployeeClass);
+
+    var comparison = MistakeDetection.compare(instructorSolution, studentSolution);
+
+    assertEquals(1, comparison.newMistakes.size());
+    assertEquals(1, studentSolution.getMistakes().size());
+
+    for (Mistake m : studentSolution.getMistakes()) {
+      assertMistake(m, USING_ASSOCIATION_INSTEAD_OF_AGGREGATION_COMPOSITION, studentMyCompanyAssociationEnd, instructorMyCompanyAssociationEnd, 0, 1,
+          false);
+    }
+  }
 }
