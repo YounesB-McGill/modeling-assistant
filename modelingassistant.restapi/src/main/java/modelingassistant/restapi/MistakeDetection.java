25965945052c9df3c2503f4d8d664cda0f5edf5b
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-07-11 02:28:53 -0400

    Add MistakeDetection REST Controller (WIP)

---
+++ modelingassistant.restapi/src/main/java/modelingassistant/restapi/MistakeDetection.java
@@ -0,0 +1,32 @@
+package modelingassistant.restapi;
+
+import static ca.mcgill.sel.mistakedetection.MistakeDetection.compare;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import modelingassistant.ModelingAssistant;
+
+public class MistakeDetection {
+  private final String modelingAssistantXmi;
+
+  public MistakeDetection(String modelingAssistantXmi) {
+    this.modelingAssistantXmi = modelingAssistantXmi;
+  }
+
+  public String getModelingAssistantXmi() {
+    return modelingAssistantXmi;
+  }
+}
+
+@RestController
+class MistakeDetectionController {
+  @GetMapping("/detectmistakes")
+  public MistakeDetection detectMistakes(@RequestParam(value = "modelingassistant") String modelingAssistantXmi) {
+    // TODO Optimize this to only detect mistakes in active problem statements
+    var modelingAssistant = ModelingAssistant.fromEcoreString(modelingAssistantXmi);
+    modelingAssistant.getProblemStatements().forEach(ps -> {
+      ps.getStudentSolutions().forEach(studentSolution -> compare(ps.getInstructorSolution(), studentSolution));
+    });
+    return new MistakeDetection(modelingAssistant.toEcoreString());
+  }
+}
