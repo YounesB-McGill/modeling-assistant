45de1057686141ecc1dbe9a3b10ced5ef26e92db
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-07-14 14:59:54 -0400

    Update comments

--- modelingassistant.restapi/src/main/java/modelingassistant/restapi/ModelingAssistantRestApi.java
+++ modelingassistant.restapi/src/main/java/modelingassistant/restapi/ModelingAssistantRestApi.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+// TODO Add authentication support
+
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // only for testing
 public class ModelingAssistantRestApi {
 
