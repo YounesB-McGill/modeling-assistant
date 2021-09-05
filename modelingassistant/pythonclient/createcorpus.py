b7925678a8ec6cf10ca0361ac61f28ac2828b517
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-09-05 01:01:13 -0400

    Migrate LC Markdown generator (untested)

--- modelingassistant/pythonclient/createcorpus.py
+++ modelingassistant/pythonclient/createcorpus.py
@@ -1,61 +1,11 @@
 #!/usr/bin/env python3
 
+from os import linesep as nl
 from fileserdes import save_to_files
 from corpus_definition import corpus
 from learningcorpus import LearningCorpus, MistakeTypeCategory
 
 MAX_NUM_OF_HASHES_IN_HEADING = 6  # See https://github.github.com/gfm/#atx-heading
-
-"""
-  /** Generates markdown for the given modeling assistant instance. */
-  public static String generate(LearningCorpus learningCorpus) {
-    var joiner = Collectors.joining("\n");
-    var topLevelMTCs = learningCorpus.getTopLevelMistakeTypeCategories();
-    return topLevelMTCs.stream().map(mtc -> getNestedTocOutputFor(mtc, 0)).collect(joiner)
-        + "\n" + topLevelMTCs.stream().map(mtc -> getNestedBodyOutputFor(mtc, 0)).collect(joiner);
-  }
-
-  /** Returns the nested table of contents output for the input in a recursive way. */
-  private static String getNestedTocOutputFor(MistakeTypeCategory mtc, int indentation) {
-    var joiner = Collectors.joining("");
-    return makeTocTitle(mtc.getName(), indentation)
-        + mtc.getSubcategories().stream().map(sc -> getNestedTocOutputFor(sc, indentation + 3)).collect(joiner)
-        + mtc.getMistakeTypes().stream().map(mt -> makeTocTitle(mt.getName(), indentation + 3)).collect(joiner);
-  }
-
-  private static String makeTocTitle(String name, int indentation) {
-    return " ".repeat(indentation) + "1. [" + name + "](#" + dashify(name) + ")\n";
-  }
-
-  /** Returns the nested body output for the input in a recursive way. */
-  private static String getNestedBodyOutputFor(MistakeTypeCategory mtc, int indentation) {
-    var joiner = Collectors.joining("\n");
-    return makeBodyTitle(mtc.getName(), indentation)
-        + mtc.getSubcategories().stream().map(sc -> getNestedBodyOutputFor(sc, indentation + 1)).collect(joiner)
-        + mtc.getMistakeTypes().stream().map(mt -> makeBodyTitle(mt.getName(), indentation + 1)).collect(joiner);
-  }
-
-  private static String makeBodyTitle(String name, int indentation) {
-    var numHashes = indentation + 2;
-    return (numHashes <= MAX_NUM_OF_HASHES_IN_HEADING?
-        "#".repeat(numHashes) + " " + clean(name) :
-        "**" + clean(name) + "**")
-        + "\n\n" ;
-  }
-
-  private static String dashify(String s) {
-    return clean(s).replace(" ", "-").replace("/", "-").replace("--", "-").toLowerCase();
-  }
-
-  private static String clean(String s) {
-    return s.replace("(", "").replace(")", "").replace(",", "").replace("yet incorrect", "");
-  }
-"""
-
-def generate_markdown(lc: LearningCorpus) -> str:
-    """
-    Generate Markdown table-of-contents from input learning corpus.
-    """
 
 
 def generate_python_mts(mtc: MistakeTypeCategory) -> str:
@@ -64,15 +14,50 @@ def generate_python_mts(mtc: MistakeTypeCategory) -> str:
     """
 
 
+def generate_markdown(lc: LearningCorpus) -> str:
+    """
+    Generate Markdown table-of-contents from input learning corpus.
+    """
+    def _nested_toc_output_for(mtc: MistakeTypeCategory, indentation: int) -> str:
+        "Return the nested table of contents output for the input in a recursive way."
+        return f'''{_make_toc_title(mtc.name, indentation)}{
+            "".join([_nested_toc_output_for(sc, indentation + 3) for sc in mtc.subcategories])}{
+            "".join([_make_toc_title(mt.name, indentation + 3) for mt in mtc.mistakeTypes])}'''
+
+    def _nested_body_output_for(mtc: MistakeTypeCategory, indentation: int) -> str:
+        "Return the nested body output for the input in a recursive way."
+        return f'''{_make_body_title(mtc.name, indentation)}{
+            "".join([_nested_body_output_for(sc, indentation + 1) for sc in mtc.subcategories])}{
+            "".join([_make_body_title(mt.name, indentation + 1) for mt in mtc.mistakeTypes])}'''
+
+    def _make_toc_title(name: str, indentation: int) -> str:
+        return f'{indentation * " "}1. [{name}](#{dashify(name)})\n'
+
+    def _make_body_title(name: str, indentation: int) -> str:
+        hashes = indentation + 2
+        cn = clean(name)
+        return (f'{hashes * "#"} {cn}' if hashes <= MAX_NUM_OF_HASHES_IN_HEADING else f'**{cn}**') + "\n\n"
+
+    return f'''{
+        nl.join([_nested_toc_output_for(mtc, 0) for mtc in lc.topLevelMistakeTypeCategories()])
+    }\n{nl.join([_nested_body_output_for(mtc, 0) for mtc in lc.topLevelMistakeTypeCategories()])}'''
+
+
+def dashify(s: str) -> str:
+    "Dashify the input string."
+    return clean(s).replace(" ", "-").replace("/", "-").replace("--", "-").lower()
+
+
+def clean(s: str) -> str:
+    "Clean the input string."
+    return s.replace("(", "").replace(")", "").replace(",", "").replace("yet incorrect", "")
+
+
 def underscorify(s: str) -> str:
     """
     Underscorifies and capitalizes the given input string, omitting any information in parentheses.
     Example: "Extra (redundant) class" -> "EXTRA_CLASS"
     """
-
-
-
-
 
 
 if __name__ == "__main__":
