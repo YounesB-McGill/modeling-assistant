2de2d327b03a5f32ec3970d4d05e126e776c5240
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2021-09-06 01:20:35 -0400

    Allow for nesting in MTC definitions to make it more DSL-like

--- modelingassistant/pythonclient/corpus.py
+++ modelingassistant/pythonclient/corpus.py
@@ -4,12 +4,18 @@ Initializes and provides access to the Learning Corpus at runtime.
 
 from corpus_definition import corpus as corpus_def, mts_by_priority as mts_by_priority_def
 from learningcorpus import LearningItem, ResourceResponse
+from utils import _mtc_subcats
 
 corpus = corpus_def
 mts_by_priority = mts_by_priority_def
 
 domain_modeling = LearningItem(name="DomainModeling")
 domain_modeling.learningCorpus = corpus
+
+for supercat, subcats in _mtc_subcats.items():
+    for subcat in subcats:
+        subcat.supercategory = supercat
+        subcat.learningCorpus = corpus
 
 for _mt in corpus.mistakeTypes():
     for feedback in _mt.feedbacks:
