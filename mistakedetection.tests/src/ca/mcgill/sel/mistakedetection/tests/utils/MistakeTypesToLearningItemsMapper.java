eceec53912bbec02d39242d6daeca7c75ba68227
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2022-01-17 20:12:14 -0500

    Update MistakeTypesToLearningItemsMapper

--- mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/utils/MistakeTypesToLearningItemsMapper.java
+++ mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/utils/MistakeTypesToLearningItemsMapper.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.eclipse.emf.ecore.EClass;
@@ -16,6 +17,7 @@ import ca.mcgill.sel.mistakedetection.Comparison;
 import ca.mcgill.sel.mistakedetection.MistakeDetectionConfig;
 import learningcorpus.MistakeType;
 import modelingassistant.Mistake;
+import modelingassistant.SolutionElement;
 
 /**
  * Class to map learning corpus mistake types to learning items.
@@ -24,11 +26,17 @@ import modelingassistant.Mistake;
  */
 public class MistakeTypesToLearningItemsMapper {
 
+  // Helper Functions to map each mistake to specific solution elements
+  static Function<Mistake, Stream<SolutionElement>> instructorElems = m -> m.getInstructorElements().stream();
+  static Function<Mistake, Stream<SolutionElement>> studentElems = m -> m.getStudentElements().stream();
+  static Function<Mistake, Stream<SolutionElement>> instructorAndStudentElems = m ->
+      Stream.concat(instructorElems.apply(m), studentElems.apply(m));
+
   public static void main(String[] args) {
     MistakeDetectionConfig.trackComparisonsInstances = true;
     runAllMistakeDetectionTests();
 
-    System.out.println(getMappingAsCsv());
+    System.out.println(getMappingAsCsv(mapComparisonMistakesToCdmMetatypes(instructorAndStudentElems)));
   }
 
   /** Runs all the mistake detection tests. */
@@ -48,11 +56,12 @@ public class MistakeTypesToLearningItemsMapper {
   }
 
   /** Maps comparison mistakes to CDM metatypes. */
-  public static Map<MistakeType, Set<EClass>> mapComparisonMistakesToCdmMetatypes() {
+  public static Map<MistakeType, Set<EClass>> mapComparisonMistakesToCdmMetatypes(
+      Function<Mistake, Stream<SolutionElement>> mistakeSolutionElementsStreamer) {
     return Comparison.instances.stream().map(comp -> comp.newMistakes).flatMap(List::stream)
         .collect(Collectors.toMap(Mistake::getMistakeType,
-            m -> Stream.concat(m.getInstructorElements().stream(), m.getStudentElements().stream())
-                .map(e -> e.getElement().eClass()).collect(Collectors.toUnmodifiableSet()), // TODO Update on Java 17+
+            m -> mistakeSolutionElementsStreamer.apply(m).map(e -> e.getElement().eClass())
+                .collect(Collectors.toUnmodifiableSet()), // TODO Update on Java 17+
             (v1, v2) -> Stream.of(v1, v2).flatMap(Set::stream).collect(Collectors.toUnmodifiableSet()),
             TreeMap::new)); // use TreeMap to sort output by mistake type
   }
@@ -63,8 +72,8 @@ public class MistakeTypesToLearningItemsMapper {
    *
    * {@code mistakeType.name: type1, type2, ...}
    */
-  public static String getMappingAsCsv() {
-    return mapComparisonMistakesToCdmMetatypes().entrySet().stream().map(e ->
+  public static String getMappingAsCsv(Map<MistakeType, Set<EClass>> mapping) {
+    return mapping.entrySet().stream().map(e ->
         e.getKey().getName() + ": " + String.join(", ", e.getValue().stream().map(EClass::getName)
             .collect(Collectors.toUnmodifiableList()))).collect(Collectors.joining("\n"));
   }
