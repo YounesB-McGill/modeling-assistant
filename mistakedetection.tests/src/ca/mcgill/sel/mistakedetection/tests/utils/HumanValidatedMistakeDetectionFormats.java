66bdcf87f013e852caec1480f4df8d0e4955712f
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2022-02-19 19:54:12 -0500

    Add MDFs for variable numbers of elements (WIP)

--- mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/utils/HumanValidatedMistakeDetectionFormats.java
+++ mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/utils/HumanValidatedMistakeDetectionFormats.java
@@ -5,6 +5,9 @@ import static java.util.Map.entry;
 // Always keep this comment here to allow easy auto-importing of new mistake types in IDEs
 //import static learningcorpus.mistaketypes.MistakeTypes.*;
 import static learningcorpus.mistaketypes.MistakeTypes.ASSOC_CLASS_SHOULD_BE_CLASS;
+import static learningcorpus.mistaketypes.MistakeTypes.ASSOC_SHOULD_BE_ENUM_PR_PATTERN;
+import static learningcorpus.mistaketypes.MistakeTypes.ASSOC_SHOULD_BE_FULL_PR_PATTERN;
+import static learningcorpus.mistaketypes.MistakeTypes.ASSOC_SHOULD_BE_SUBCLASS_PR_PATTERN;
 import static learningcorpus.mistaketypes.MistakeTypes.ATTRIBUTE_DUPLICATED;
 import static learningcorpus.mistaketypes.MistakeTypes.ATTRIBUTE_MISPLACED;
 import static learningcorpus.mistaketypes.MistakeTypes.ATTRIBUTE_MISPLACED_IN_GENERALIZATION_HIERARCHY;
@@ -18,7 +21,11 @@ import static learningcorpus.mistaketypes.MistakeTypes.BAD_ENUM_NAME_SPELLING;
 import static learningcorpus.mistaketypes.MistakeTypes.BAD_ROLE_NAME_SPELLING;
 import static learningcorpus.mistaketypes.MistakeTypes.CLASS_SHOULD_BE_ASSOC_CLASS;
 import static learningcorpus.mistaketypes.MistakeTypes.CLASS_SHOULD_BE_ENUM;
+import static learningcorpus.mistaketypes.MistakeTypes.COMPOSED_PART_CONTAINED_IN_MORE_THAN_ONE_PARENT;
+import static learningcorpus.mistaketypes.MistakeTypes.ENUM_SHOULD_BE_ASSOC_PR_PATTERN;
 import static learningcorpus.mistaketypes.MistakeTypes.ENUM_SHOULD_BE_CLASS;
+import static learningcorpus.mistaketypes.MistakeTypes.ENUM_SHOULD_BE_FULL_PR_PATTERN;
+import static learningcorpus.mistaketypes.MistakeTypes.ENUM_SHOULD_BE_SUBCLASS_PR_PATTERN;
 import static learningcorpus.mistaketypes.MistakeTypes.EXTRA_AGGREGATION;
 import static learningcorpus.mistaketypes.MistakeTypes.EXTRA_ASSOCIATION;
 import static learningcorpus.mistaketypes.MistakeTypes.EXTRA_ASSOC_CLASS;
@@ -28,8 +35,13 @@ import static learningcorpus.mistaketypes.MistakeTypes.EXTRA_COMPOSITION;
 import static learningcorpus.mistaketypes.MistakeTypes.EXTRA_ENUM;
 import static learningcorpus.mistaketypes.MistakeTypes.EXTRA_ENUM_ITEM;
 import static learningcorpus.mistaketypes.MistakeTypes.EXTRA_GENERALIZATION;
+import static learningcorpus.mistaketypes.MistakeTypes.FULL_PR_PATTERN_SHOULD_BE_ASSOC;
+import static learningcorpus.mistaketypes.MistakeTypes.FULL_PR_PATTERN_SHOULD_BE_ENUM;
+import static learningcorpus.mistaketypes.MistakeTypes.FULL_PR_PATTERN_SHOULD_BE_SUBCLASS;
 import static learningcorpus.mistaketypes.MistakeTypes.GENERALIZATION_SHOULD_BE_ASSOC_AO_PATTERN;
 import static learningcorpus.mistaketypes.MistakeTypes.INCOMPLETE_AO_PATTERN;
+import static learningcorpus.mistaketypes.MistakeTypes.INCOMPLETE_CONTAINMENT_TREE;
+import static learningcorpus.mistaketypes.MistakeTypes.INCOMPLETE_PR_PATTERN;
 import static learningcorpus.mistaketypes.MistakeTypes.INFINITE_RECURSIVE_DEPENDENCY;
 import static learningcorpus.mistaketypes.MistakeTypes.LOWERCASE_CLASS_NAME;
 import static learningcorpus.mistaketypes.MistakeTypes.MISSING_AGGREGATION;
@@ -50,6 +62,9 @@ import static learningcorpus.mistaketypes.MistakeTypes.REPRESENTING_ACTION_WITH_
 import static learningcorpus.mistaketypes.MistakeTypes.ROLE_SHOULD_BE_STATIC;
 import static learningcorpus.mistaketypes.MistakeTypes.ROLE_SHOULD_NOT_BE_STATIC;
 import static learningcorpus.mistaketypes.MistakeTypes.SOFTWARE_ENGINEERING_TERM;
+import static learningcorpus.mistaketypes.MistakeTypes.SUBCLASS_SHOULD_BE_ASSOC_PR_PATTERN;
+import static learningcorpus.mistaketypes.MistakeTypes.SUBCLASS_SHOULD_BE_ENUM_PR_PATTERN;
+import static learningcorpus.mistaketypes.MistakeTypes.SUBCLASS_SHOULD_BE_FULL_PR_PATTERN;
 import static learningcorpus.mistaketypes.MistakeTypes.UPPERCASE_ATTRIBUTE_NAME;
 import static learningcorpus.mistaketypes.MistakeTypes.USING_AGGREGATION_INSTEAD_OF_COMPOSITION;
 import static learningcorpus.mistaketypes.MistakeTypes.USING_ASSOC_INSTEAD_OF_AGGREGATION;
@@ -74,7 +89,7 @@ public class HumanValidatedMistakeDetectionFormats {
 
   public static final Map<MistakeType, MistakeDetectionFormat> mappings = Map.ofEntries(
       // paste entries from MDIS4LC here (studentElemsDescriptions, instructorElemsDescriptions)
-      // already done
+      // already done (derived from MDS)
       entry(ASSOC_CLASS_SHOULD_BE_CLASS, mdf(List.of("cls"), List.of("cls"))),
       entry(ATTRIBUTE_DUPLICATED, mdf(List.of("attr"), List.of())),
       entry(ATTRIBUTE_MISPLACED, mdf(List.of("attr"), List.of("attr"))),
@@ -135,26 +150,33 @@ public class HumanValidatedMistakeDetectionFormats {
       entry(WRONG_GENERALIZATION_DIRECTION, mdf(List.of("super_cls", "sub_cls"), List.of("sub_cls", "super_cls"))),
       entry(WRONG_MULTIPLICITY, mdf(List.of("assocend"), List.of("assocend"))),
       entry(WRONG_ROLE_NAME, mdf(List.of("assocend"), List.of("assocend"))),
-      entry(WRONG_SUPERCLASS, mdf(List.of("sub_cls", "super_cls"), List.of("sub_cls", "super_cls")))/*,
+      entry(WRONG_SUPERCLASS, mdf(List.of("sub_cls", "super_cls"), List.of("sub_cls", "super_cls"))),
 
-      // in progress
+      // TODO Prabhsimran: Ensure the MDS returns these results by unit testing or other means
+
+      entry(ASSOC_SHOULD_BE_ENUM_PR_PATTERN, mdf(List.of("player_cls", "role_assocend*"), List.of("player_cls", "role_attr"))),
+      entry(ASSOC_SHOULD_BE_FULL_PR_PATTERN, mdf(List.of("player_cls", "role_assocend*"), List.of("player_cls", "role_cls*"))),
+      entry(ASSOC_SHOULD_BE_SUBCLASS_PR_PATTERN, mdf(List.of("player_cls", "role_assocend*"), List.of("player_cls", "role_cls*"))),
+
+      entry(COMPOSED_PART_CONTAINED_IN_MORE_THAN_ONE_PARENT, mdf(List.of("cls*"), List.of())),
+
+      entry(ENUM_SHOULD_BE_ASSOC_PR_PATTERN, mdf(List.of("player_cls", "role_enumitem*"), List.of("player_cls", "role_assocend*"))),
+      entry(ENUM_SHOULD_BE_FULL_PR_PATTERN, mdf(List.of("player_cls", "role_enumitem*"), List.of("player_cls", "role_cls*"))),
+      entry(ENUM_SHOULD_BE_SUBCLASS_PR_PATTERN, mdf(List.of("player_cls", "role_enumitem*"), List.of("player_cls", "role_cls*"))),
+
+      entry(FULL_PR_PATTERN_SHOULD_BE_ASSOC, mdf(List.of("player_cls", "role_cls*"), List.of("player_cls", "role_assocend*"))),
+      entry(FULL_PR_PATTERN_SHOULD_BE_ENUM, mdf(List.of("player_cls", "role_cls*"), List.of("player_cls", "role_attr"))),
+      entry(FULL_PR_PATTERN_SHOULD_BE_SUBCLASS, mdf(List.of("player_cls", "role_cls*"), List.of("player_cls", "role_cls*"))),
+
+      entry(INCOMPLETE_PR_PATTERN, mdf(List.of("cls*"), List.of("cls*"))), // TODO Double check
+
+      entry(INCOMPLETE_CONTAINMENT_TREE, mdf(List.of("cls*"), List.of())),
+
+      entry(SUBCLASS_SHOULD_BE_ASSOC_PR_PATTERN, mdf(List.of("player_cls", "role_cls*"), List.of("player_cls", "role_assocend*"))),
+      entry(SUBCLASS_SHOULD_BE_ENUM_PR_PATTERN, mdf(List.of("player_cls", "role_cls*"), List.of("player_cls", "role_attr"))),
+      entry(SUBCLASS_SHOULD_BE_FULL_PR_PATTERN, mdf(List.of("player_cls", "role_cls*"), List.of("player_cls", "role_cls*")))
 
       // to be done later
-      //entry(ASSOC_SHOULD_BE_ENUM_PR_PATTERN, mdf(List.of("0_compos", "1_assocend", "2_assocend", "3_cls"), List.of("0_cls", "1_attr"))),
-      //entry(ASSOC_SHOULD_BE_FULL_PR_PATTERN, mdf(List.of("0_compos", "1_assocend", "2_assocend", "3_cls"), List.of("0_cls", "1_cls", "2_cls"))),
-      //entry(ASSOC_SHOULD_BE_SUBCLASS_PR_PATTERN, mdf(List.of("0_compos", "1_assocend", "2_assocend", "3_cls"), List.of("0_cls", "1_cls", "2_cls"))),
-      //entry(COMPOSED_PART_CONTAINED_IN_MORE_THAN_ONE_PARENT, mdf(List.of("0_cls", "1_cls"), List.of())),
-      //entry(ENUM_SHOULD_BE_ASSOC_PR_PATTERN, mdf(List.of("0_enumitem", "1_enumitem", "2_cls"), List.of("0_cls", "1_assocend", "2_assocend"))),
-      //entry(ENUM_SHOULD_BE_FULL_PR_PATTERN, mdf(List.of("0_enumitem", "1_enumitem", "2_cls"), List.of("0_cls", "1_cls", "2_cls"))),
-      //entry(ENUM_SHOULD_BE_SUBCLASS_PR_PATTERN, mdf(List.of("0_enumitem", "1_enumitem", "2_cls"), List.of("0_cls", "1_cls", "2_cls"))),
-      //entry(FULL_PR_PATTERN_SHOULD_BE_ASSOC, mdf(List.of("0_cls", "1_cls", "2_cls"), List.of("0_cls", "1_assocend", "2_assocend"))),
-      //entry(FULL_PR_PATTERN_SHOULD_BE_ENUM, mdf(List.of("0_cls", "1_cls", "2_cls"), List.of("0_cls", "1_attr"))),
-      //entry(FULL_PR_PATTERN_SHOULD_BE_SUBCLASS, mdf(List.of("0_cls", "1_cls", "2_cls"), List.of("0_cls", "1_cls", "2_cls"))),
-      //entry(INCOMPLETE_PR_PATTERN, mdf(List.of("0_cls", "1_cls"), List.of("0_cls", "1_cls", "2_cls"))),
-      //entry(INCOMPLETE_CONTAINMENT_TREE, mdf(List.of("0_cls", "1_cls", "2_cls", "3_cls", "4_cls", "5_cls", "6_cls", "7_cls", "8_cls", "9_cls", "10_cls"), List.of())),
-      //entry(SUBCLASS_SHOULD_BE_ASSOC_PR_PATTERN, mdf(List.of("0_cls", "1_cls", "2_cls"), List.of("0_cls", "1_assocend", "2_assocend"))),
-      //entry(SUBCLASS_SHOULD_BE_ENUM_PR_PATTERN, mdf(List.of("0_cls", "1_cls", "2_cls"), List.of("0_cls", "1_attr"))),
-      //entry(SUBCLASS_SHOULD_BE_FULL_PR_PATTERN, mdf(List.of("0_cls", "1_cls", "2_cls"), List.of("0_cls", "1_cls", "2_cls"))),*/
   );
 
 }
