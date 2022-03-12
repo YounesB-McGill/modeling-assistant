684c0c31258e57121ae803c72618f97b4c756e2b
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2022-03-11 22:38:42 -0500

    Update MDFs to match latest MDS changes (only a few left)

--- mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/utils/HumanValidatedMistakeDetectionFormats.java
+++ mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/utils/HumanValidatedMistakeDetectionFormats.java
@@ -20,8 +20,10 @@ import static learningcorpus.mistaketypes.MistakeTypes.BAD_CLASS_NAME_SPELLING;
 import static learningcorpus.mistaketypes.MistakeTypes.BAD_ENUM_ITEM_SPELLING;
 import static learningcorpus.mistaketypes.MistakeTypes.BAD_ENUM_NAME_SPELLING;
 import static learningcorpus.mistaketypes.MistakeTypes.BAD_ROLE_NAME_SPELLING;
+import static learningcorpus.mistaketypes.MistakeTypes.CLASS_SHOULD_BE_ABSTRACT;
 import static learningcorpus.mistaketypes.MistakeTypes.CLASS_SHOULD_BE_ASSOC_CLASS;
 import static learningcorpus.mistaketypes.MistakeTypes.CLASS_SHOULD_BE_ENUM;
+import static learningcorpus.mistaketypes.MistakeTypes.CLASS_SHOULD_NOT_BE_ABSTRACT;
 import static learningcorpus.mistaketypes.MistakeTypes.COMPOSED_PART_CONTAINED_IN_MORE_THAN_ONE_PARENT;
 import static learningcorpus.mistaketypes.MistakeTypes.ENUM_SHOULD_BE_ASSOC_PR_PATTERN;
 import static learningcorpus.mistaketypes.MistakeTypes.ENUM_SHOULD_BE_CLASS;
@@ -120,32 +122,34 @@ public class HumanValidatedMistakeDetectionFormats {
       entry(BAD_ENUM_ITEM_SPELLING, mdf(List.of("enumitem"), List.of("enumitem"))),
       entry(BAD_ENUM_NAME_SPELLING, mdf(List.of("enum"), List.of("enum"))),
       entry(BAD_ROLE_NAME_SPELLING, mdf(List.of("assocend"), List.of("assocend"))),
+      entry(CLASS_SHOULD_BE_ABSTRACT, mdf(List.of("cls"), List.of("cls"))),
       entry(CLASS_SHOULD_BE_ASSOC_CLASS, mdf(List.of("cls"), List.of("assoc", "cls"))),
       entry(CLASS_SHOULD_BE_ENUM, mdf(List.of("cls"), List.of("enum"))),
+      entry(CLASS_SHOULD_NOT_BE_ABSTRACT, mdf(List.of("cls"), List.of("cls"))),
       entry(ENUM_SHOULD_BE_CLASS, mdf(List.of("enum"), List.of("cls"))),
-      entry(EXTRA_AGGREGATION, mdf(List.of("aggr"), List.of())),
+      entry(EXTRA_AGGREGATION, mdf(List.of("aggr", "part_assocend", "whole_assocend"), List.of())),
       entry(EXTRA_ASSOC_CLASS, mdf(List.of("assoc", "cls"), List.of())),
       entry(EXTRA_ASSOCIATION, mdf(List.of("assoc"), List.of())),
-      entry(EXTRA_ATTRIBUTE, mdf(List.of("attr"), List.of())),
+      entry(EXTRA_ATTRIBUTE, mdf(List.of("attr"), List.of(/*"cls"*/))), // TODO double check
       entry(EXTRA_CLASS, mdf(List.of("cls"), List.of())),
-      entry(EXTRA_COMPOSITION, mdf(List.of("compos"), List.of())),
+      entry(EXTRA_COMPOSITION, mdf(List.of("compos", "part_assocend", "whole_assocend"), List.of())),
       entry(EXTRA_ENUM, mdf(List.of("enum"), List.of())),
-      entry(EXTRA_ENUM_ITEM, mdf(List.of("enumitem"), List.of())),
-      entry(EXTRA_GENERALIZATION, mdf(List.of("sub_cls", "super_cls"), List.of("sub_cls", "super_cls"))),
+      entry(EXTRA_ENUM_ITEM, mdf(List.of("enumitem"), List.of(/*"enum" ?*/))), // TODO double check
+      entry(EXTRA_GENERALIZATION, mdf(List.of("sub_cls", "super_cls"), List.of(/*"sub_cls", "super_cls"*/))), // TODO double check
       entry(GENERALIZATION_SHOULD_BE_ASSOC_AO_PATTERN, mdf(List.of("sub_cls", "super_cls"), List.of("abs_cls", "occ_cls"))),
       entry(INCOMPLETE_AO_PATTERN, mdf(List.of("abs_cls", "occ_cls"), List.of("abs_cls", "occ_cls"))),
       entry(INFINITE_RECURSIVE_DEPENDENCY, mdf(List.of("minlowerbound_assocend", "other_assocend"), List.of())),
       entry(LOWERCASE_CLASS_NAME, mdf(List.of("cls"), List.of("cls"))),
-      entry(MISSING_AGGREGATION, mdf(List.of(), List.of("part_assocend", "whole_assocend"))), // TODO update MDS
+      entry(MISSING_AGGREGATION, mdf(List.of(), List.of("aggr", "part_assocend", "whole_assocend"))),
       entry(MISSING_AO_PATTERN, mdf(List.of(), List.of("abs_cls", "occ_cls"))),
       entry(MISSING_ASSOC_CLASS, mdf(List.of(), List.of("assoc", "cls"))),
       entry(MISSING_ASSOCIATION, mdf(List.of(), List.of("assoc"))),
-      entry(MISSING_ATTRIBUTE, mdf(List.of(), List.of("attr"))),
+      entry(MISSING_ATTRIBUTE, mdf(List.of("cls"), List.of("attr"))),
       entry(MISSING_CLASS, mdf(List.of(), List.of("cls"))),
-      entry(MISSING_COMPOSITION, mdf(List.of(), List.of("part_assocend", "whole_assocend"))), // TODO update MDS
+      entry(MISSING_COMPOSITION, mdf(List.of(), List.of("compos", "part_assocend", "whole_assocend"))),
       entry(MISSING_ENUM, mdf(List.of(), List.of("enum"))),
-      entry(MISSING_ENUM_ITEM, mdf(List.of(), List.of("enumitem"))),
-      entry(MISSING_GENERALIZATION, mdf(List.of("sub_cls", "super_cls"), List.of())),
+      entry(MISSING_ENUM_ITEM, mdf(List.of("enum"), List.of("enumitem"))),
+      entry(MISSING_GENERALIZATION, mdf(List.of("sub_cls", "super_cls"), List.of("sub_cls", "super_cls"))),
       entry(MISSING_ROLE_NAMES, mdf(List.of("assocend"), List.of("assocend"))),
       entry(NON_DIFFERENTIATED_SUBCLASS, mdf(List.of("cls"), List.of())),
       entry(PLURAL_ATTRIBUTE, mdf(List.of("attr"), List.of("attr"))),
@@ -155,18 +159,20 @@ public class HumanValidatedMistakeDetectionFormats {
       entry(ROLE_SHOULD_NOT_BE_STATIC, mdf(List.of("assocend"), List.of("assocend"))),
       entry(SOFTWARE_ENGINEERING_TERM, mdf(List.of("cls"), List.of("cls"))),
       entry(UPPERCASE_ATTRIBUTE_NAME, mdf(List.of("attr"), List.of("attr"))),
-      entry(USING_AGGREGATION_INSTEAD_OF_COMPOSITION, mdf(List.of("assocend"), List.of("assocend"))),
-      entry(USING_ASSOC_INSTEAD_OF_AGGREGATION, mdf(List.of("assocend"), List.of("assocend"))),
-      entry(USING_ASSOC_INSTEAD_OF_COMPOSITION, mdf(List.of("assocend"), List.of("assocend"))),
+      entry(USING_AGGREGATION_INSTEAD_OF_ASSOC, mdf(List.of("aggr", "part_assocend", "whole_assocend"), List.of("assoc", "assocend", "other_assocend"))),
+      entry(USING_AGGREGATION_INSTEAD_OF_COMPOSITION, mdf(List.of("aggr", "part_assocend", "whole_assocend"), List.of("compos", "part_assocend", "whole_assocend"))),
+      entry(USING_ASSOC_INSTEAD_OF_AGGREGATION, mdf(List.of("assoc", "assocend", "other_assocend"), List.of("aggr", "part_assocend", "whole_assocend"))),
+      entry(USING_ASSOC_INSTEAD_OF_COMPOSITION, mdf(List.of("assoc", "assocend", "other_assocend"), List.of("compos", "part_assocend", "whole_assocend"))),
       entry(USING_ATTRIBUTE_INSTEAD_OF_ASSOC, mdf(List.of("attr"), List.of("assocend"))),
-      entry(USING_COMPOSITION_INSTEAD_OF_AGGREGATION, mdf(List.of("assocend"), List.of("assocend"))),
-      entry(USING_COMPOSITION_INSTEAD_OF_ASSOC, mdf(List.of("part_assocend", "whole_assocend"), List.of("assoc"))), // TODO double check then update MDS
-      entry(USING_DIRECTED_RELATIONSHIP_INSTEAD_OF_UNDIRECTED, mdf(List.of("assocend"), List.of("assocend"))),
-      entry(USING_UNDIRECTED_RELATIONSHIP_INSTEAD_OF_DIRECTED, mdf(List.of("source_assocend", "target_assocend"), List.of("source_assocend", "target_assocend"))), // to be discussed
+      entry(USING_COMPOSITION_INSTEAD_OF_AGGREGATION, mdf(List.of("compos", "part_assocend", "whole_assocend"), List.of("aggr", "part_assocend", "whole_assocend"))),
+      entry(USING_COMPOSITION_INSTEAD_OF_ASSOC, mdf(List.of("compos", "part_assocend", "whole_assocend"), List.of("assoc", "assocend", "other_assocend"))),
+      entry(USING_DIRECTED_RELATIONSHIP_INSTEAD_OF_UNDIRECTED, mdf(List.of("aggr_compos_or_assoc", "source_assocend", "target_assocend"), List.of("aggr_compos_or_assoc", "assocend", "other_assocend"))),
+      entry(USING_UNDIRECTED_RELATIONSHIP_INSTEAD_OF_DIRECTED, mdf(List.of("aggr_compos_or_assoc", "assocend", "other_assocend"), List.of("aggr_compos_or_assoc", "source_assocend", "target_assocend"))),
       entry(WRONG_ATTRIBUTE_TYPE, mdf(List.of("attr"), List.of("attr"))),
       entry(WRONG_CLASS_NAME, mdf(List.of("cls"), List.of("cls"))),
       entry(WRONG_GENERALIZATION_DIRECTION, mdf(List.of("super_cls", "sub_cls"), List.of("sub_cls", "super_cls"))),
       entry(WRONG_MULTIPLICITY, mdf(List.of("assocend"), List.of("assocend"))),
+      entry(WRONG_RELATIONSHIP_DIRECTION, mdf(List.of("aggr_compos_or_assoc", "source_assocend", "target_assocend"), List.of("aggr_compos_or_assoc", "source_assocend", "target_assocend"))),
       entry(WRONG_ROLE_NAME, mdf(List.of("assocend"), List.of("assocend"))),
       entry(WRONG_SUPERCLASS, mdf(List.of("sub_cls", "super_cls"), List.of("sub_cls", "super_cls"))),
 
@@ -192,8 +198,6 @@ public class HumanValidatedMistakeDetectionFormats {
       // In progress: MDS logic implemented (or will be implemented soon) but no tests
       entry(LIST_ATTRIBUTE, mdf(List.of("attr"), List.of("attr"))),
       entry(MISSING_PR_PATTERN, mdf(List.of(), List.of(""))), // TODO [], [missingElements]
-      entry(USING_AGGREGATION_INSTEAD_OF_ASSOC, mdf(List.of("assocend"), List.of("assocend"))),
-      entry(WRONG_RELATIONSHIP_DIRECTION, mdf(List.of(""), List.of(""))), // TODO
 
       // Future work: mistake detection not implemented for these mistake types
       // TODO complete student/instructor descriptions
