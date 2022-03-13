e107dea3f829f0e0f13e024af969453183825e94
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2022-03-13 18:00:06 -0400

    Update assocends in cdmmetatypes to match cdm metamodel

--- modelingassistant/pythonclient/cdmmetatypes.py
+++ modelingassistant/pythonclient/cdmmetatypes.py
@@ -39,7 +39,7 @@ Miminal Umple model used for examples:
 
 namespace Airline;
 
-class AirlineSystem{ 1 <@>- * Person persons; }
+class AirlineSystem{ 1 airlineSystem <@>- * Person persons; }
 
 class PersonRole {}
 class PassengerRole {
@@ -52,7 +52,7 @@ class VisitorRole { isA PersonRole; }
 class Person {
   String name;
   Integer idNumber;
-  1 -- 0..3 PersonRole roles;
+  1 person -- 0..3 PersonRole roles;
 }
 
 class Booking {
@@ -60,13 +60,13 @@ class Booking {
   SeatType seatType;
 }
 
-//$?[End_of_model]$?
 namespace -;
 // LAYOUT INFORMATION
 class Person {
-  position 426 126 152 75;
+  position 460 121 152 75;
   position.association Person__PersonRole 0,18 110,19;
   position.association Person__PersonRole:roles 0,10 110,10;
+  position.association Person:person__PersonRole:roles 0,37 110,10;
 }
 class PersonRole { position 251 168 109 41;}
 class Booking { position 116 382 109 75; }
@@ -75,9 +75,10 @@ class PassengerRole {
   position.association Booking__PassengerRole 28,70 27,0;
 }
 class AirlineSystem {
-  position 628 26 115.25 41;
+  position 478 16 115.25 41;
   position.association AirlineSystem__Person 46,0 75,80;
   position.association AirlineSystem__Person:persons 0,10 153,2;
+  position.association AirlineSystem:airlineSystem__Person:persons 43,41 61,0;
 }
 class EmployeeRole { position 291 278 116.812 41; }
 class VisitorRole { position 450 279 109 41; }
@@ -87,7 +88,7 @@ _MANY = -1
 _cd_string = CDString()
 _cd_int = CDInt()
 
-_airline_system = Class(name="AirlineSystem")
+_airlinesystem = Class(name="AirlineSystem")
 _person = Class(name="Person", attributes=[
     _person_name := Attribute(name="name", type=_cd_string), _person_id := Attribute(name="idNumber", type=_cd_int)])
 _person_role = Class(name="PersonRole")
@@ -98,13 +99,28 @@ _booking = Class(name="Booking", attributes=[Attribute(name="seatType", type=(_s
     name="SeatType", literals=[_first_class := CDEnumLiteral(name="FirstClass"),
                                CDEnumLiteral(name="Business"), CDEnumLiteral(name="Economy")])))])
 
-_airline_system_persons = Association(ends=[
-    _airline_system_composend := ae(_airline_system, 1, 1, ReferenceType.Composition),
-    ae(_person, 0, _MANY, n="persons")])
-_person_roles = Association(ends=[ae(_person, 1, 1), _roles := ae(_person_role, 0, 3, n="roles")])
+"""
+Associations and association ends and how they connect to classes in the CDM metamodel can be unintuitive.
+Using example from above:
+
+class AirlineSystem{ 1 airlineSystem <@>- * Person persons; } // an AirlineSystem contains persons
+
+There are five concepts defined as follows, according to the CDM metamodel:
+
+  1. The AirlineSystem class
+  2. The Person class
+  3. The association between the AirlineSystem and the Person class. Conceptually, this is a composition
+  4. The persons association end, which is contained in the AirlineSystem class and has Composition reference type.
+     Note that this is the opposite of where the composition filled diamond is drawn in the diagram!
+  5. The airlineSystem association end, which is contained in the Person class and has a Regular reference type.
+"""
+_airlinesystem_persons = Association(ends=[
+    _persons_composend := ae(_airlinesystem, 0, _MANY, ReferenceType.Composition, n="persons"),
+    _airlinesystem_assocend := ae(_person, 1, 1, n="airlineSystem")])
+_person_roles = Association(ends=[ae(_person, 0, 3, n="roles"), _roles := ae(_person_role, 1, 1, n="person")])
 _passengerroles_bookings = Association(ends=[
-    _passenger_aggrend := ae(_passenger_role, 1, 1, ReferenceType.Aggregation, "passenger"),
-    ae(_booking, 0, _MANY, n="bookings")])
+    _bookings_aggrend := ae(_passenger_role, 0, _MANY, ReferenceType.Aggregation, "bookings"),
+    ae(_booking, 1, 1, n="passenger")])
 
 
 CDM_METATYPES = {
@@ -112,12 +128,12 @@ CDM_METATYPES = {
     "assoc": (assoc := CdmMetatype("assoc", "Association", Association, _person_roles)),
     "assocend": (assocend := CdmMetatype("assocend", "Association End", AssociationEnd, _roles)),
     "assocend*": (assocends := CdmMetatype("assocend*", "Association Ends", AssociationEnd,
-                                           [_roles, _airline_system_composend, _passenger_aggrend])),
+                                           [_roles, _persons_composend, _airlinesystem_assocend, _bookings_aggrend])),
     "attr": (attr := CdmMetatype("attr", "Attribute", Attribute, _person_name)),
     "attr*": (attrs := CdmMetatype("attr*", "Attributes", Attribute, [_person_name, _person_id])),
     "cls": (cls := CdmMetatype("cls", "Class", Class, _person)),
     "cls*": (clss := CdmMetatype("cls*", "Classes", Class, [_passenger_role, _employee_role, _visitor_role])),
-    "compos": (compos := CdmMetatype("compos", "Composition", Association, _airline_system_persons)),
+    "compos": (compos := CdmMetatype("compos", "Composition", Association, _airlinesystem_persons)),
     "enum": (enum := CdmMetatype("enum", "Enumeration", CDEnum, _seat_type)),
     "enumitem": (enumitem := CdmMetatype("enumitem", "Enumeration Item", CDEnumLiteral, _first_class)),
     "enumitem*": (enumitems := CdmMetatype("enumitem*", "Enumeration Items", CDEnumLiteral, _seat_type.literals)),
