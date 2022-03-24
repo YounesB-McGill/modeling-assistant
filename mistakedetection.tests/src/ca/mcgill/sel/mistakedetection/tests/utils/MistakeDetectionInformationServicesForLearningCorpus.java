0d6b0fa3e82842d6d167b0bb34dad4d9053e849a
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2022-03-24 00:33:13 -0400

    Restore deleted imports from MDIS4LC (broken starting in PR #72)

--- mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/utils/MistakeDetectionInformationServicesForLearningCorpus.java
+++ mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/utils/MistakeDetectionInformationServicesForLearningCorpus.java
@@ -11,9 +11,11 @@ import ca.mcgill.sel.mistakedetection.MistakeDetectionConfig;
 import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.ColorDemo;
 import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.MappingToCdmMetatypes;
 import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.MappingToElementDescriptions;
+import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.MappingToLearningCorpusElementTypes;
 import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.MappingToMistakeDetectionFormats;
 import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.MappingToMistakeDetectionFormatsForPython;
 import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.MappingToMistakeInfos;
+import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.MistakeDetectionFormatComparison;
 import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.MistakeDetectionInformationService;
 import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.PythonLearningCorpusInitializationCode;
 import ca.mcgill.sel.mistakedetection.tests.utils.infoservice.SourceTargetVerifier;
@@ -61,10 +63,12 @@ public class MistakeDetectionInformationServicesForLearningCorpus {
         MappingToElementDescriptions.get(),
         MappingToMistakeInfos.get(),
         SourceTargetVerifier.get(),
+        MappingToLearningCorpusElementTypes.get(),
         MappingToMistakeDetectionFormats.get(),
         SuggestedParametrizedResponsesForJava.get(),
         SuggestedParametrizedResponses.get(),
         MappingToMistakeDetectionFormatsForPython.get(),
+        MistakeDetectionFormatComparison.get(),
         PythonLearningCorpusInitializationCode.get()
         ).map(MistakeDetectionInformationService::getOutput);
 
