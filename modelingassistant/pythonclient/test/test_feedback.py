f83fbf72d36de839367dcf68bb57cc9fd24652a4
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2022-04-13 06:21:55 -0400

    Update unit tests to test feedback for wrong multiplicity

--- modelingassistant/pythonclient/test/test_feedback.py
+++ modelingassistant/pythonclient/test/test_feedback.py
@@ -51,7 +51,7 @@ def make_ma_without_mistakes() -> ModelingAssistant:
     return ma
 
 
-def make_ma_with_1_sw_eng_terms_mistake(num_detection: int=1) -> ModelingAssistant:
+def make_ma_with_1_sw_eng_term_mistake(num_detection: int=1) -> ModelingAssistant:
     """
     Make a simple Modeling Assistant instance with one Software engineering term mistake, detected `num_detection`
     times.
@@ -68,7 +68,7 @@ def make_ma_with_1_sw_eng_terms_mistake(num_detection: int=1) -> ModelingAssista
     return ma
 
 
-def make_ma_with_1_wrong_mult_terms_mistake(num_detection: int=1) -> ModelingAssistant:
+def make_ma_with_1_wrong_mult_mistake(num_detection: int=1) -> ModelingAssistant:
     """
     Make a simple Modeling Assistant instance with one Wrong multiplicity mistake, detected `num_detection` times.
     """
@@ -101,13 +101,13 @@ def test_feedback_without_mistakes():
     assert "no mistakes found" in feedback_item.feedback.text.lower()
 
 
-def test_feedback_with_1_mistake_level_1():
+def test_feedback_with_1_sw_eng_term_mistake_level_1():
     """
     Test feedback for a solution with one mistake made a first time.
 
     BusData detected for first time -> highlight BusData
     """
-    ma = make_ma_with_1_sw_eng_terms_mistake(1)
+    ma = make_ma_with_1_sw_eng_term_mistake(1)
     solution = ma.problemStatements[0].studentSolutions[0]
     feedback_item = give_feedback(solution)
     curr_mistake = feedback_item.mistake
@@ -122,13 +122,13 @@ def test_feedback_with_1_mistake_level_1():
     assert 9 == ma.studentKnowledges[0].levelOfKnowledge
 
 
-def test_feedback_with_1_mistake_level_2():
+def test_feedback_with_1_sw_eng_term_mistake_level_2():
     """
     Test feedback for a solution with one mistake made a second time.
 
     BusData (or similar) detected for second time -> text response
     """
-    ma = make_ma_with_1_sw_eng_terms_mistake(2)
+    ma = make_ma_with_1_sw_eng_term_mistake(2)
     solution = ma.problemStatements[0].studentSolutions[0]
     feedback_item = give_feedback(solution)
     curr_mistake = feedback_item.mistake
@@ -144,13 +144,13 @@ def test_feedback_with_1_mistake_level_2():
     assert 8 == ma.studentKnowledges[0].levelOfKnowledge
 
 
-def test_feedback_with_1_mistake_level_3():
+def test_feedback_with_1_sw_eng_term_mistake_level_3():
     """
     Test feedback for a solution with one mistake made a third time.
 
     BusData (or similar) detected for third time -> parameterized response
     """
-    ma = make_ma_with_1_sw_eng_terms_mistake(3)
+    ma = make_ma_with_1_sw_eng_term_mistake(3)
     solution = ma.problemStatements[0].studentSolutions[0]
     feedback_item = give_feedback(solution)
     curr_mistake = feedback_item.mistake
@@ -167,13 +167,13 @@ def test_feedback_with_1_mistake_level_3():
     assert 7 == ma.studentKnowledges[0].levelOfKnowledge
 
 
-def test_feedback_with_1_mistake_level_4():
+def test_feedback_with_1_sw_eng_term_mistake_level_4():
     """
     Test feedback for a solution with one mistake made a fourth time.
 
     BusData (or similar) detected for fourth time -> resource response with example
     """
-    ma = make_ma_with_1_sw_eng_terms_mistake(4)
+    ma = make_ma_with_1_sw_eng_term_mistake(4)
     solution = ma.problemStatements[0].studentSolutions[0]
     feedback_item = give_feedback(solution)
     curr_mistake = feedback_item.mistake
@@ -191,13 +191,13 @@ def test_feedback_with_1_mistake_level_4():
     assert 6 == ma.studentKnowledges[0].levelOfKnowledge
 
 
-def test_feedback_with_1_mistake_levels_1_4():
+def test_feedback_with_1_sw_eng_term_mistake_levels_1_4():
     """
     Test feedback for a solution with one mistake made four times in a row.
 
     BusData detected 4 times -> 4 levels of feedback, given one at a time.
     """
-    ma = make_ma_with_1_sw_eng_terms_mistake(1)
+    ma = make_ma_with_1_sw_eng_term_mistake(1)
     solution = ma.problemStatements[0].studentSolutions[0]
     feedback_item = give_feedback(solution)
     curr_mistake = feedback_item.mistake
@@ -254,13 +254,156 @@ def test_feedback_with_1_mistake_levels_1_4():
     assert 6 == ma.studentKnowledges[0].levelOfKnowledge
 
 
+def test_feedback_with_1_wrong_mult_mistake_level_1():
+    """
+    Test feedback for a solution with one mistake made a first time.
+
+    Wrong multiplicity detected 1 time -> highlight solution
+    """
+    ma = make_ma_with_1_wrong_mult_mistake(1)
+    solution = ma.problemStatements[0].studentSolutions[0]
+    feedback_item = give_feedback(solution)
+    curr_mistake = feedback_item.mistake
+    wrong_mult_sk = next(sk for sk in ma.studentKnowledges if sk.mistakeType is curr_mistake.mistakeType)
+
+    assert feedback_item.solution is solution
+    assert curr_mistake.lastFeedback is feedback_item
+    feedback = feedback_item.feedback
+    assert isinstance(feedback, Feedback)
+    assert 1 == feedback.level
+    assert feedback.highlightSolution
+    assert curr_mistake.mistakeType is feedback.mistakeType
+    wrong_mult_sk = next(sk for sk in ma.studentKnowledges if sk.mistakeType is curr_mistake.mistakeType)
+    assert 9 == wrong_mult_sk.levelOfKnowledge
+
+
+def test_feedback_with_1_wrong_mult_mistake_level_2():
+    """
+    Test feedback for a solution with one mistake made a second time.
+
+    Wrong multiplicity detected 2 times -> text response
+    """
+    ma = make_ma_with_1_wrong_mult_mistake(2)
+    solution = ma.problemStatements[0].studentSolutions[0]
+    feedback_item = give_feedback(solution)
+    curr_mistake = feedback_item.mistake
+    wrong_mult_sk = next(sk for sk in ma.studentKnowledges if sk.mistakeType is curr_mistake.mistakeType)
+
+    assert feedback_item.solution is solution
+    assert curr_mistake.lastFeedback is feedback_item
+    feedback = feedback_item.feedback
+    assert isinstance(feedback, TextResponse)
+    assert 2 == feedback.level
+    assert not feedback.highlightSolution
+    assert "Double check this association" in feedback.text
+    assert curr_mistake.mistakeType is feedback.mistakeType
+    assert 8 == wrong_mult_sk.levelOfKnowledge
+
+
+def test_feedback_with_1_wrong_mult_mistake_level_3():
+    """
+    Test feedback for a solution with one mistake made a third time.
+
+    Wrong multiplicity detected 3 times -> more detailed text response
+    """
+    ma = make_ma_with_1_wrong_mult_mistake(3)
+    solution = ma.problemStatements[0].studentSolutions[0]
+    feedback_item = give_feedback(solution)
+    curr_mistake = feedback_item.mistake
+    wrong_mult_sk = next(sk for sk in ma.studentKnowledges if sk.mistakeType is curr_mistake.mistakeType)
+
+    assert feedback_item.solution is solution
+    assert curr_mistake.lastFeedback is feedback_item
+    feedback = feedback_item.feedback
+    assert isinstance(feedback, TextResponse)
+    assert 3 == feedback.level
+    assert not feedback.highlightSolution
+    assert "multiplicity" in feedback.text
+    assert curr_mistake.mistakeType is feedback.mistakeType
+    assert 7 == wrong_mult_sk.levelOfKnowledge
+
+
+def test_feedback_with_1_wrong_mult_mistake_level_4():
+    """
+    Test feedback for a solution with one mistake made a fourth time.
+
+    Wrong multiplicity detected 4 times -> parametrized response
+    """
+    ma = make_ma_with_1_wrong_mult_mistake(4)
+    solution = ma.problemStatements[0].studentSolutions[0]
+    feedback_item = give_feedback(solution)
+    curr_mistake = feedback_item.mistake
+    wrong_mult_sk = next(sk for sk in ma.studentKnowledges if sk.mistakeType is curr_mistake.mistakeType)
+
+    assert feedback_item.solution is solution
+    assert curr_mistake.lastFeedback is feedback_item
+    feedback = feedback_item.feedback
+    assert isinstance(feedback, ParametrizedResponse)
+    assert 4 == feedback.level
+    assert not feedback.highlightSolution
+    assert "instances" in feedback.text
+    assert "Garage" in feedback_item.text
+    assert curr_mistake.mistakeType is feedback.mistakeType
+    assert 6 == wrong_mult_sk.levelOfKnowledge
+
+
+def test_feedback_with_1_wrong_mult_mistake_level_5():
+    """
+    Test feedback for a solution with one mistake made a fifth time.
+
+    Wrong multiplicity detected 5 times -> resource response with quiz
+    """
+    ma = make_ma_with_1_wrong_mult_mistake(5)
+    solution = ma.problemStatements[0].studentSolutions[0]
+    feedback_item = give_feedback(solution)
+    curr_mistake = feedback_item.mistake
+    wrong_mult_sk = next(sk for sk in ma.studentKnowledges if sk.mistakeType is curr_mistake.mistakeType)
+
+    assert feedback_item.solution is solution
+    assert curr_mistake.lastFeedback is feedback_item
+    feedback = feedback_item.feedback
+    assert isinstance(feedback, ResourceResponse)
+    assert isinstance(feedback.learningResources[0], Quiz)
+    assert 5 == feedback.level
+    assert not feedback.highlightSolution
+    resource_content = feedback.learningResources[0].content
+    assert "Pick the association" in resource_content
+    assert curr_mistake.mistakeType is feedback.mistakeType
+    assert 5 == wrong_mult_sk.levelOfKnowledge
+
+
+def test_feedback_with_1_wrong_mult_mistake_level_6():
+    """
+    Test feedback for a solution with one mistake made a sixth time.
+
+    Wrong multiplicity detected 6 times -> resource response with reference
+    """
+    ma = make_ma_with_1_wrong_mult_mistake(6)
+    solution = ma.problemStatements[0].studentSolutions[0]
+    feedback_item = give_feedback(solution)
+    curr_mistake = feedback_item.mistake
+    wrong_mult_sk = next(sk for sk in ma.studentKnowledges if sk.mistakeType is curr_mistake.mistakeType)
+
+    assert feedback_item.solution is solution
+    assert curr_mistake.lastFeedback is feedback_item
+    feedback = feedback_item.feedback
+    assert isinstance(feedback, ResourceResponse)
+    assert isinstance(feedback.learningResources[0], Reference)
+    assert 6 == feedback.level
+    assert not feedback.highlightSolution
+    resource_content = feedback.learningResources[0].content
+    assert "multiplicities" in resource_content
+    assert curr_mistake.mistakeType is feedback.mistakeType
+    assert 4 == wrong_mult_sk.levelOfKnowledge
+
+
 def test_feedback_with_1_mistake_levels_1_6():
     """
-    Test feedback for a solution with one mistake made four times in a row.
+    Test feedback for a solution with one mistake made six times in a row.
 
-    BusData detected 4 times -> 4 levels of feedback, given one at a time.
+    Wrong multiplicity detected 6 times -> 6 levels of feedback, given one at a time.
     """
-    ma = make_ma_with_1_wrong_mult_terms_mistake(1)
+    ma = make_ma_with_1_wrong_mult_mistake(1)
     solution = ma.problemStatements[0].studentSolutions[0]
     feedback_item = give_feedback(solution)
     curr_mistake = feedback_item.mistake
