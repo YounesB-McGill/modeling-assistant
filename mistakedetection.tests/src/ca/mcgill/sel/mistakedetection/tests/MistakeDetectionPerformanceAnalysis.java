f2431bc71b6f4b1737bf2b8228615be785a067bc
Prabhsimran Singh <prabhsimran98@gmail.com>
2022-05-11 20:26:30 -0400

    added synonyms to instructor solution of case study

--- mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/MistakeDetectionPerformanceAnalysis.java
+++ mistakedetection.tests/src/ca/mcgill/sel/mistakedetection/tests/MistakeDetectionPerformanceAnalysis.java
@@ -3,8 +3,12 @@ package ca.mcgill.sel.mistakedetection.tests;
 import static ca.mcgill.sel.mistakedetection.tests.MistakeDetectionTest.instructorSolutionFromClassDiagram;
 import static ca.mcgill.sel.mistakedetection.tests.MistakeDetectionTest.studentSolutionFromClassDiagram;
 import static modelingassistant.util.ResourceHelper.cdmFromFile;
+import static modelingassistant.util.SynonymUtils.setSynonymToAttribInClassInClassDiag;
+import static modelingassistant.util.SynonymUtils.setSynonymToClassInClassDiag;
+import static modelingassistant.util.SynonymUtils.setSynonymToRoleInClassInClassDiag;
 import static modelingassistant.util.TagUtils.setAbstractionTagToClassInClassDiag;
 import static modelingassistant.util.TagUtils.setOccurrenceTagToClassInClassDiag;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import ca.mcgill.sel.classdiagram.ClassDiagram;
 import ca.mcgill.sel.mistakedetection.MistakeDetection;
@@ -31,6 +35,35 @@ public class MistakeDetectionPerformanceAnalysis extends MistakeDetectionBaseTes
     instructorSolution = instructorSolutionFromClassDiagram(instructorClassDiagram);
     TagGroup tagGroup = setAbstractionTagToClassInClassDiag("RoomType", instructorClassDiagram, instructorSolution);
     setOccurrenceTagToClassInClassDiag("Room", tagGroup, instructorClassDiagram);
+
+    // Assigning synonyms to classes
+    setSynonymToClassInClassDiag("Person", List.of("Guest"), instructorClassDiagram, instructorSolution);
+    setSynonymToClassInClassDiag("Booking", List.of("Reservation", "RoomReservation"), instructorClassDiagram, instructorSolution);
+    setSynonymToClassInClassDiag("Hotel", List.of("HotelSocs", "SOCSHotel", "HotelSystem"), instructorClassDiagram, instructorSolution);
+
+    // Assigning synonyms to attributes
+    setSynonymToAttribInClassInClassDiag("RoomType","maxDailyRate", List.of("maxRate", "maximumDailyRate"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("RoomType","qualityLevel", List.of("quality", "level"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Room","roomNumber", List.of("number"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Room","status", List.of("smokingStatus", "smoking"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Bed","type", List.of("bedKind", "bedType"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Person","phoneNumber", List.of("telephone", "phone", "telephoneNumber"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Person","creditcard", List.of("card", "cardNumber", "creditCardNumber"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Stay","checkinDate", List.of("checkin", "date"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Stay","numberOfNights", List.of("nights", "duration", "length"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Stay","dailyRate", List.of("nightlyRate", "rate", "price", "currentRate"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Booking","startDate", List.of("start", "date"), instructorClassDiagram, instructorSolution);
+    setSynonymToAttribInClassInClassDiag("Booking","bookedDailyRate", List.of("dailyRate"), instructorClassDiagram, instructorSolution);
+
+    // Assigning synonyms to assoc ends
+    setSynonymToRoleInClassInClassDiag("Hotel", "bookings", List.of("reservations", "myReservations"), instructorClassDiagram, instructorSolution);
+    setSynonymToRoleInClassInClassDiag("Room", "bookings", List.of("reservations", "myReservations"), instructorClassDiagram, instructorSolution);
+    setSynonymToRoleInClassInClassDiag("Room", "hotel", List.of("myHotelSystem", "inHotel", "mySOCSHotel"), instructorClassDiagram, instructorSolution);
+    setSynonymToRoleInClassInClassDiag("Person", "bookings", List.of("reservations", "myReservations","myRoomReservations"), instructorClassDiagram, instructorSolution);
+    setSynonymToRoleInClassInClassDiag("Person", "hotel", List.of("myHotelSystem", "inHotel", "mySOCSHotel"), instructorClassDiagram, instructorSolution);
+    setSynonymToRoleInClassInClassDiag("Booking", "bookedRooms", List.of("reservedRooms"), instructorClassDiagram, instructorSolution);
+    setSynonymToRoleInClassInClassDiag("Booking", "By", List.of("reservedBy"), instructorClassDiagram, instructorSolution);
+    setSynonymToRoleInClassInClassDiag("Booking", "hotel", List.of("myHotelSystem", "inHotel", "mySOCSHotel"), instructorClassDiagram, instructorSolution);
   }
   @Test
   public void testStudentSolution1() {
