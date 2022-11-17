46222970739fe7bfa3140bd2970f834341f171ff
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2022-11-17 01:03:22 -0500

    Regenerate PyEcore model code

--- modelingassistant/pythonapp/modelingassistant/modelingassistant.py
+++ modelingassistant/pythonapp/modelingassistant/modelingassistant.py
@@ -265,7 +265,7 @@ def override_pyecorevalue_check(self, value, _isinstance=isinstance):
         if etype in (EPackage, EClassifier, EString) or isinstance(value, EProxy) or value.eClass.name == etype.__name__ or (etype.__name__ in [c.name for c in value.eClass.eSuperTypes]):
             return True
         for _module in [classdiagram, learningcorpus, __import__(__name__)]:
-            for (name, cls) in inspect.getmembers(_module, inspect.isclass):
+            for name, cls in inspect.getmembers(_module, inspect.isclass):
                 if name == value.eClass.name and etype.name in (c.__name__ for c in cls.__bases__):
                     return True
         raise BadValueError(got=value, expected=etype, feature=feature)
