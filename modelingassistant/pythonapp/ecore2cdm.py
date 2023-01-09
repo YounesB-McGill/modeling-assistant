6a46114899ed02015c63b0038936718dc8c83370
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2023-01-08 19:20:53 -0500

    Add support for compositions (via Umple)

--- modelingassistant/pythonapp/ecore2cdm.py
+++ modelingassistant/pythonapp/ecore2cdm.py
@@ -7,6 +7,7 @@ Author: Younes Boubekeur
 """
 
 import os
+import string
 import sys
 
 from pyecore.ecore import EAttribute, EClass, EDataType, EObject, EPackage, EReference
@@ -14,6 +15,7 @@ from pyecore.resources import ResourceSet, URI
 
 from classdiagram import Association, AssociationEnd, Attribute, CDEnum, Class, ClassDiagram
 from fileserdes import save_to_file
+from utils import warn
 
 
 def load_ecore_model(ecore_file: str) -> EPackage:
@@ -25,12 +27,12 @@ def load_ecore_model(ecore_file: str) -> EPackage:
     return mm_root
 
 
-def convert(ecore_file: str):
+def convert(ecore_file: str) -> ClassDiagram:
     "Create a TouchCORE cdm file obtained by applying a model transformation on the input Ecore file."
     print(f"Converting {ecore_file}")
     ecore_model = load_ecore_model(ecore_file)
     cdm = ClassDiagram(name=ecore_model.name)
-    cdm_items: dict[str, EObject] = {ecore_model.name: cdm}  # keep track of cdm items by name
+    cdm_items: dict[str, EObject] = {ecore_model.name: cdm}  # track cdm items by name (assocend names not unique)
     for class_ in ecore_model.eClassifiers:
         match class_:
             case EDataType(name=enum_name):  # enum class
@@ -53,8 +55,10 @@ def convert(ecore_file: str):
                             assocend = AssociationEnd(name=assocend_name, classifier=cls, lowerBound=lb, upperBound=ub,
                                                       assoc=assoc_for(eref, cdm, cdm_items))
                             cdm_items[assocend_name] = assocend
-
-    save_to_file(f"{ecore_file.removesuffix('.ecore')}.cdm", cdm)
+    ecore_file_base_name = ecore_file.removesuffix('.ecore')
+    cdm = enhance_with_umple_file_info(cdm, f"{ecore_file_base_name}.ump", cdm_items)
+    save_to_file(f"{ecore_file_base_name}.cdm", cdm)
+    return cdm
 
 
 def assoc_for(eref: EReference, cdm: ClassDiagram, cdm_items: dict[str, EObject]) -> Association:
@@ -73,12 +77,13 @@ def assoc_for(eref: EReference, cdm: ClassDiagram, cdm_items: dict[str, EObject]
         """
         cls1, cls2 = sorted((eref.eContainingClass.name, eref.eType.name))
         # ensure that the order of the association ends is never flipped
+        opposite_name = getattr(eref.eOpposite, "name", "")  # to support unidirectional associations
         if eref.eContainingClass.name < eref.eType.name:
-            ae1, ae2 = eref.eOpposite.name, eref.name
+            ae1, ae2 = opposite_name, eref.name
         elif eref.eContainingClass.name > eref.eType.name:
-            ae1, ae2 = eref.name, eref.eOpposite.name
+            ae1, ae2 = eref.name, opposite_name
         else:  # both class names are equal, so it's a reflexive association
-            ae1, ae2 = sorted((eref.name, eref.eOpposite.name))
+            ae1, ae2 = sorted((eref.name, opposite_name))
         return ",".join((cls1, cls2, ae1, ae2))
 
     name = unique_name(eref)
@@ -92,7 +97,7 @@ def assoc_for(eref: EReference, cdm: ClassDiagram, cdm_items: dict[str, EObject]
     return cdm_items[name]
 
 
-def enhance_with_umple_file_info(cdm: ClassDiagram, umple_file: str, cdm_items: dict[str, EObject]):
+def enhance_with_umple_file_info(cdm: ClassDiagram, umple_file: str, cdm_items: dict[str, EObject]) -> ClassDiagram:
     """
     Enhance the given TouchCORE diagram obtained from an Ecore file with additional information based on its source
     Umple file, if it exists.
@@ -102,10 +107,58 @@ def enhance_with_umple_file_info(cdm: ClassDiagram, umple_file: str, cdm_items: 
 
     - Composition
     - Class is abstract
-    - Class is an interface
+    - Class is an interface, which also implies that superclasses need to be handled here
     - Enumeration items (Enumerations themselves are supported)
     """
-    # to be completed in the future
+    if not os.path.isfile(umple_file):
+        warn(f'The file "{umple_file}" does not exist, so its CDM remains unchanged')
+        return cdm
+    with open(umple_file, "r", encoding="utf-8") as f:
+        umple_code = f.read().split("//$?[End_of_model]$?")[0]
+    umple_lines = ["", *umple_code.splitlines()]  # Add a dummy "line" at the beginning to have 1-indexed lines
+    if umple_lines[-1]:
+        umple_lines.append("")  # always end with a newline
+    num_lines = len(umple_lines) - 1
+
+    # detect compositions
+    for i in range(1, num_lines + 1):
+        if "<@>" in umple_lines[i]:
+            # replace "-" with " " and remove special characters
+            compos_line = umple_lines[i].translate(str.maketrans("-", " ", f"{string.digits}*.<@>;{{}}")).strip()
+            """
+            Example:
+
+            class Car {
+              1 car <@>- * Wheel wheels;
+            }
+
+            whole = Car, whole_refcls = Wheel.car, part = Wheel, part_refcls = Car.wheels
+            """
+            print(f"{compos_line = }")
+            j = i
+            while j:
+                if umple_lines[j].lstrip().startswith("class "):
+                    break
+                j -= 1
+            whole = umple_lines[j].replace("class ", "").replace("{", "").replace("}", "").strip()
+            compos_line_components = compos_line.split()
+            part, part_refcls = compos_line_components[-2:]
+            whole_refcls = (compos_line_components[0] if len(compos_line_components) == 3
+                            else f"{whole[0].lower()}{whole[1:]}")
+            if whole < part:
+                assoc_name = ",".join((whole, part, whole_refcls, part_refcls))
+            elif whole > part:
+                assoc_name = ",".join((part, whole, part_refcls, whole_refcls))
+            else:
+                warn(f"Found reflexive association in {umple_file}: {whole} <@>- {part}")
+            if assoc_name in cdm_items:
+                assoc: Association = cdm_items[assoc_name]
+                # assume that each composition has exactly 2 ends
+                composend: AssociationEnd = assoc.ends[0] if assoc.ends[0].classifier.name == whole else assoc.ends[1]
+                composend.referenceType = "Composition"
+            else:
+                warn(f'Could not find the "{assoc_name}" association in cdm_items dictionary')
+    return cdm
 
 
 def main():
