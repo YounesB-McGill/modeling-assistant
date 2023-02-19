eec792af1754b6fa1fa7c6c9dc09331c113288ce
Younes Boubekeur <younes.boubekeur@mail.mcgill.ca>
2023-02-18 23:27:49 -0500

    Add mds_evaluation_helper.get_spreadsheet_results()

--- modelingassistant/pythonapp/test/mds_evaluation_helper.py
+++ modelingassistant/pythonapp/test/mds_evaluation_helper.py
@@ -5,9 +5,60 @@ results in a common spreadsheet.
 
 Author: Younes Boubekeur
 """
+# pylint: disable=wrong-import-position
+
+import sys
+import os
 
 from openpyxl import load_workbook
 
+# this line is needed to be able to call this script directly
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from envvars import env_vars
 
+
 SPREADSHEET_LOCATIONS: str = env_vars["mds-evaluation-spreadsheet-location"]
+COMMON_SPREADSHEET_PATH = f"{SPREADSHEET_LOCATIONS}/common-spreadsheet.xlsx"
+
+INSTRUCTOR_SOLUTION_IDS = (201, 202, 203, 204)
+EVALUATED_STUDENT_SOLUTION_IDS = [14, 36, 58, 80, 97]
+
+TN_TP_FP_FN_R_P_F1_F2 = tuple[int, int, int, int, float, float, float, float]  # pylint: disable=invalid-name
+
+
+def produce_common_spreadsheet():
+    "Produce the common results spreadsheet from the individual ones used for the evaluation."
+    for instructor_solution_id in INSTRUCTOR_SOLUTION_IDS:
+        for student_solution_id in EVALUATED_STUDENT_SOLUTION_IDS:
+            print(get_spreadsheet_results(student_solution_id, instructor_solution_id))
+
+
+def get_spreadsheet_results(student_solution_id, instructor_solution_id) -> TN_TP_FP_FN_R_P_F1_F2:
+    """
+    Return the 8-tuple with the following elements for the given submission_id:
+
+    - True Negatives
+    - True Positives
+    - False Positives
+    - False Negatives
+    - Recall (TP / (TP + FN))
+    - Precision (TP / (TP + FP))
+    - F1 (2 * Precision * Recall / (Precision + Recall))
+    - F2 (5 * Precision * Recall) / (4 * Precision + Recall)
+    """
+    result = []
+    ws = load_workbook(f"{SPREADSHEET_LOCATIONS}/{student_solution_id}/"
+                       f"GroundTruth_{student_solution_id}_{instructor_solution_id}.xlsx",
+                       read_only=True, data_only=True).active
+    reached_tn = False
+    for row in ws.values:
+        if not reached_tn and row[0] and row[0].startswith("True Negative"):
+            reached_tn = True
+        if reached_tn:
+            result.append(row[2] if row[2] is not None else 1)  # 1 is a default value if actual value is unavailable
+    return tuple(result)
+
+
+if __name__ == "__main__":
+    produce_common_spreadsheet()
